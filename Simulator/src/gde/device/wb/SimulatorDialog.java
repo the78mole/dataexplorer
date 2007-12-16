045dceb0b33b7c6277b46cab70d499ddcb1cf1fa
Winfried Bruegmann <winfried.bruegmann@web.de>
2007-12-16 21:07:01 +0000

    add dispose listener for cleanup purpose

--- Simulator/src/gde/device/wb/SimulatorDialog.java
+++ Simulator/src/gde/device/wb/SimulatorDialog.java
@@ -11,6 +11,8 @@ import java.util.logging.Logger;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FormAttachment;
@@ -95,140 +97,153 @@ public class SimulatorDialog extends DeviceDialog {
 	 */
 	public void open() {
 		try {
-			Shell parent = getParent();
-			dialogShell = new Shell(parent, SWT.NULL); // SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL
-			SWTResourceManager.registerResourceUser(dialogShell);
-			dialogShell.setLayout(new FormLayout());
-			dialogShell.layout();
-			dialogShell.pack();
-			dialogShell.setSize(336, 393);
-			dialogShell.setText("Simulator");
-			dialogShell.setImage(SWTResourceManager.getImage("osde/resource/Tools.gif"));
-			{
-				FormData descriptionLData = new FormData();
-				descriptionLData.width = 277;
-				descriptionLData.height = 60;
-				descriptionLData.left = new FormAttachment(0, 1000, 24);
-				descriptionLData.top = new FormAttachment(0, 1000, 87);
-				description = new Text(dialogShell, SWT.CENTER | SWT.WRAP);
-				description.setLayoutData(descriptionLData);
-				description.setText("Mit START wird eine neue Serie an Daten generiert, bis STOP gedrückt wird");
-				description.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
-			}
-			{
-				FormData descriptionLabelLData = new FormData();
-				descriptionLabelLData.width = 304;
-				descriptionLabelLData.height = 37;
-				descriptionLabelLData.left = new FormAttachment(0, 1000, 12);
-				descriptionLabelLData.top = new FormAttachment(0, 1000, 12);
-				descriptionLabelLData.right = new FormAttachment(1000, 1000, -12);
-				descriptionLabel = new CLabel(dialogShell, SWT.CENTER | SWT.WRAP | SWT.EMBEDDED);
-				descriptionLabel.setLayoutData(descriptionLabelLData);
-				descriptionLabel.setText("Datengenerator");
-				descriptionLabel.setFont(SWTResourceManager.getFont("Microsoft Sans Serif", 14, 0, false, false));
-			}
-			{
-				FormData startButtonLData = new FormData();
-				startButtonLData.width = 198;
-				startButtonLData.height = 31;
-				startButtonLData.left = new FormAttachment(0, 1000, 62);
-				startButtonLData.top = new FormAttachment(0, 1000, 203);
-				startButton = new Button(dialogShell, SWT.PUSH | SWT.CENTER);
-				startButton.setLayoutData(startButtonLData);
-				startButton.setText("START");
-				startButton.addSelectionListener(new SelectionAdapter() {
-					public void widgetSelected(SelectionEvent evt) {
-						System.out.println("startButton.widgetSelected, event=" + evt);
-						channel = channels.getActiveChannel();
-						channels.switchChannel(channel.getName());
+			log.fine("dialogShell.isDisposed() " + ((dialogShell == null) ? "null" : dialogShell.isDisposed()));
+			if (dialogShell == null || dialogShell.isDisposed()) {
+				Shell parent = getParent();
+				dialogShell = new Shell(parent, SWT.DIALOG_TRIM); // !SWT.APPLICATION_MODAL
+				SWTResourceManager.registerResourceUser(dialogShell);
+				dialogShell.setLayout(new FormLayout());
+				dialogShell.layout();
+				dialogShell.pack();
+				dialogShell.setSize(336, 393);
+				dialogShell.setText("Simulator");
+				dialogShell.setImage(SWTResourceManager.getImage("osde/resource/Tools.gif"));
+				dialogShell.addDisposeListener(new DisposeListener() {
+					public void widgetDisposed(DisposeEvent evt) {
+						log.fine("dialogShell.widgetDisposed, event=" + evt);
+						//TODO check if some thing to do before exiting
+					}
+				});
+				{
+					FormData descriptionLData = new FormData();
+					descriptionLData.width = 277;
+					descriptionLData.height = 60;
+					descriptionLData.left = new FormAttachment(0, 1000, 24);
+					descriptionLData.top = new FormAttachment(0, 1000, 87);
+					description = new Text(dialogShell, SWT.CENTER | SWT.WRAP);
+					description.setLayoutData(descriptionLData);
+					description.setText("Mit START wird eine neue Serie an Daten generiert, bis STOP gedrückt wird");
+					description.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
+				}
+				{
+					FormData descriptionLabelLData = new FormData();
+					descriptionLabelLData.width = 304;
+					descriptionLabelLData.height = 37;
+					descriptionLabelLData.left = new FormAttachment(0, 1000, 12);
+					descriptionLabelLData.top = new FormAttachment(0, 1000, 12);
+					descriptionLabelLData.right = new FormAttachment(1000, 1000, -12);
+					descriptionLabel = new CLabel(dialogShell, SWT.CENTER | SWT.WRAP | SWT.EMBEDDED);
+					descriptionLabel.setLayoutData(descriptionLabelLData);
+					descriptionLabel.setText("Datengenerator");
+					descriptionLabel.setFont(SWTResourceManager.getFont("Microsoft Sans Serif", 14, 0, false, false));
+				}
+				{
+					FormData startButtonLData = new FormData();
+					startButtonLData.width = 198;
+					startButtonLData.height = 31;
+					startButtonLData.left = new FormAttachment(0, 1000, 62);
+					startButtonLData.top = new FormAttachment(0, 1000, 203);
+					startButton = new Button(dialogShell, SWT.PUSH | SWT.CENTER);
+					startButton.setLayoutData(startButtonLData);
+					startButton.setText("START");
+					startButton.addSelectionListener(new SelectionAdapter() {
+						public void widgetSelected(SelectionEvent evt) {
+							System.out.println("startButton.widgetSelected, event=" + evt);
+							channel = channels.getActiveChannel();
+							channels.switchChannel(channel.getName());
 
-						// prepare timed data gatherer thread
-						int delay = 0;
-						int period = device.getTimeStep_ms() * device.getClusterSize();
-						log.fine("timer period = " + period + " ms");
-						timer = new Timer();
-						timerTask = new TimerTask() {
-							String									recordSetKeyStem	= ") Datensatz";
-							HashMap<String, Object>	data;															// Spannung, Strom
-							String									recordSetKey;
+							// prepare timed data gatherer thread
+							int delay = 0;
+							int period = device.getTimeStep_ms() * device.getClusterSize();
+							log.fine("timer period = " + period + " ms");
+							timer = new Timer();
+							timerTask = new TimerTask() {
+								String									recordSetKeyStem	= ") Datensatz";
+								HashMap<String, Object>	data;															// Spannung, Strom
+								String									recordSetKey;
 
-							@SuppressWarnings("unchecked")
-							//(Vector<Integer>)
-							public void run() {
-								RecordSet recordSet;
-								try {
+								@SuppressWarnings("unchecked")
+								//(Vector<Integer>)
+								public void run() {
+									RecordSet recordSet;
+									try {
 
-									data = serialPort.getData(null, recordNumber, null);
+										data = serialPort.getData(null, recordNumber, null);
 
-									if (channel.size() == 0 || isCollectDataStopped) {
-										isCollectDataStopped = false;
-										recordNumber++;
+										if (channel.size() == 0 || isCollectDataStopped) {
+											isCollectDataStopped = false;
+											recordNumber++;
 
-										recordSetKey = (channel.size() + 1) + recordSetKeyStem;
-										channel.put(recordSetKey, RecordSet.createRecordSet(recordSetKey, application.getActiveDevice(), true, false));
-										log.fine(recordSetKey + " created for channel " + channel.getName());
-										if (channel.getActiveRecordSet() == null) Channels.getInstance().getActiveChannel().setActiveRecordSet(recordSetKey);
-										channel.get(recordSetKey).setAllDisplayable();
-										channel.applyTemplate(recordSetKey);
+											recordSetKey = (channel.size() + 1) + recordSetKeyStem;
+											channel.put(recordSetKey, RecordSet.createRecordSet(recordSetKey, application.getActiveDevice(), true, false));
+											log.fine(recordSetKey + " created for channel " + channel.getName());
+											if (channel.getActiveRecordSet() == null) Channels.getInstance().getActiveChannel().setActiveRecordSet(recordSetKey);
+											channel.get(recordSetKey).setAllDisplayable();
+											channel.applyTemplate(recordSetKey);
 
-										// switch the active record set if the current record set is child of active channel
-										if (channel.getName().equals(channels.getActiveChannel().getName())) {
-											application.getDataToolBar().addRecordSetName(recordSetKey);
-											channels.getActiveChannel().getActiveRecordSet().switchRecordSet(recordSetKey);
+											// switch the active record set if the current record set is child of active channel
+											if (channel.getName().equals(channels.getActiveChannel().getName())) {
+												application.getDataToolBar().addRecordSetName(recordSetKey);
+												channels.getActiveChannel().getActiveRecordSet().switchRecordSet(recordSetKey);
+											}
 										}
-									}
-									else {
-										log.fine("re-using " + recordSetKey);
-									}
+										else {
+											log.fine("re-using " + recordSetKey);
+										}
 
-									// prepare the data for adding to record set
-									recordSet = channel.get(recordSetKey);
-									// build the point array according curves from record set
-									int[] points = new int[recordSet.size()];
+										// prepare the data for adding to record set
+										recordSet = channel.get(recordSetKey);
+										// build the point array according curves from record set
+										int[] points = new int[recordSet.size()];
 
-									Vector<Integer> voltage = (Vector<Integer>) data.get(RecordSet.VOLTAGE);
-									Vector<Integer> current = (Vector<Integer>) data.get(RecordSet.CURRENT);
-									Iterator<Integer> iterV = voltage.iterator();
-									Iterator<Integer> iterA = current.iterator();
-									while (iterV.hasNext()) {
-										points[0] = iterV.next().intValue();//Spannung 
-										points[1] = iterA.next().intValue();//Strom 
-										log.fine(String.format("Spannung = %d mV, Strom = %d mA", points[0], points[1]));
-										recordSet.addPoints(points, false);
+										Vector<Integer> voltage = (Vector<Integer>) data.get(RecordSet.VOLTAGE);
+										Vector<Integer> current = (Vector<Integer>) data.get(RecordSet.CURRENT);
+										Iterator<Integer> iterV = voltage.iterator();
+										Iterator<Integer> iterA = current.iterator();
+										while (iterV.hasNext()) {
+											points[0] = iterV.next().intValue();//Spannung 
+											points[1] = iterA.next().intValue();//Strom 
+											log.fine(String.format("Spannung = %d mV, Strom = %d mA", points[0], points[1]));
+											recordSet.addPoints(points, false);
+										}
+
+										application.updateGraphicsWindow();
+										//application.updateDataTable();
+										//application.updateDigitalWindow();
 									}
-
-									application.updateGraphicsWindow();
-									//application.updateDataTable();
-									//application.updateDigitalWindow();
+									catch (IOException e) {
+										e.printStackTrace();
+									}
 								}
-								catch (IOException e) {
-									e.printStackTrace();
-								}
-							}
-						};
-						timer.scheduleAtFixedRate(timerTask, delay, period);
-					}
-				});
+							};
+							timer.scheduleAtFixedRate(timerTask, delay, period);
+						}
+					});
+				}
+				{
+					FormData okButtonLData = new FormData();
+					okButtonLData.width = 203;
+					okButtonLData.height = 33;
+					okButtonLData.left = new FormAttachment(0, 1000, 62);
+					okButtonLData.top = new FormAttachment(0, 1000, 273);
+					stopButton = new Button(dialogShell, SWT.PUSH | SWT.CENTER);
+					stopButton.setLayoutData(okButtonLData);
+					stopButton.setText("STOP");
+					stopButton.addSelectionListener(new SelectionAdapter() {
+						public void widgetSelected(SelectionEvent evt) {
+							log.fine("stopButton.widgetSelected, event=" + evt);
+							stopTimer();
+							isCollectDataStopped = true;
+						}
+					});
+				}
+				dialogShell.setLocation(getParent().toDisplay(100, 100));
+				dialogShell.open();
 			}
-			{
-				FormData okButtonLData = new FormData();
-				okButtonLData.width = 203;
-				okButtonLData.height = 33;
-				okButtonLData.left = new FormAttachment(0, 1000, 62);
-				okButtonLData.top = new FormAttachment(0, 1000, 273);
-				stopButton = new Button(dialogShell, SWT.PUSH | SWT.CENTER);
-				stopButton.setLayoutData(okButtonLData);
-				stopButton.setText("STOP");
-				stopButton.addSelectionListener(new SelectionAdapter() {
-					public void widgetSelected(SelectionEvent evt) {
-						log.fine("stopButton.widgetSelected, event=" + evt);
-						stopTimer();
-						isCollectDataStopped = true;
-					}
-				});
+			else {
+				dialogShell.setVisible(true);
+				dialogShell.setActive();
 			}
-			dialogShell.setLocation(getParent().toDisplay(100, 100));
-			dialogShell.open();
 			Display display = dialogShell.getDisplay();
 			while (!dialogShell.isDisposed()) {
 				if (!display.readAndDispatch()) display.sleep();
