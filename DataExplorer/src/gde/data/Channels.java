0573e8eac28cfdde5b8d268aaddee4f414274df6
Winfried Bruegmann <winfried.bruegmann@web.de>
2008-01-21 20:46:08 +0000

    fix error if a gatherer thread switch to its  actual channel

--- DataExplorer/src/gde/data/Channels.java
+++ DataExplorer/src/gde/data/Channels.java
@@ -78,7 +78,8 @@ public class Channels extends HashMap<Integer, Channel> {
 	 * @param channelName assuming p.e. " 1 : Ausgang"
 	 */
 	public synchronized void switchChannel(String channelName) {
-		this.getActiveChannel().getActiveRecordSet().reset();
+		RecordSet recordSet = this.getActiveChannel().getActiveRecordSet();
+		if (recordSet != null) recordSet.reset();
 		this.switchChannel(new Integer(channelName.split(":")[0].trim()).intValue());
 	}
 
@@ -88,7 +89,8 @@ public class Channels extends HashMap<Integer, Channel> {
 	 */
 	public synchronized void switchChannel(int channelNumber) {
 		log.fine("switching to channel " + channelNumber);		
-		this.getActiveChannel().getActiveRecordSet().reset();
+		RecordSet recordSet = this.getActiveChannel().getActiveRecordSet();
+		if (recordSet != null) recordSet.reset();
 		if (channelNumber != this.getActiveChannelNumber()) {
 			this.setActiveChannelNumber(channelNumber);
 			application.getMenuToolBar().updateChannelToolItems();
