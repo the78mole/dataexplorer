2db37c310bdc53a45f21b5e8686ddf750aab2ced
Winfried Bruegmann <winfried.bruegmann@web.de>
2008-06-29 13:13:29 +0000

    implement eStation charger support

---
+++ eStation/src/gde/device/bantam/eStationBC6.java
@@ -0,0 +1,68 @@
+package osde.device.bantam;
+
+
+import java.io.FileNotFoundException;
+import java.util.logging.Logger;
+
+import javax.xml.bind.JAXBException;
+
+import osde.device.DeviceConfiguration;
+import osde.device.IDevice;
+
+/**
+ * Sample device class, used as template for new device implementations
+ * @author Winfried Brügmann
+ */
+public class eStationBC6 extends eStation implements IDevice {
+	final static Logger						log	= Logger.getLogger(eStationBC6.class.getName());
+
+	/**
+	 * constructor using properties file
+	 * @throws JAXBException 
+	 * @throws FileNotFoundException 
+	 */
+	public eStationBC6(String deviceProperties) throws FileNotFoundException, JAXBException {
+		super(deviceProperties);
+		this.dialog = new EStationDialog(this.application.getShell(), this);
+	}
+
+	/**
+	 * constructor using existing device configuration
+	 * @param deviceConfig device configuration
+	 */
+	public eStationBC6(DeviceConfiguration deviceConfig) {
+		super(deviceConfig);
+		this.dialog = new EStationDialog(this.application.getShell(), this);
+	}
+
+	/**
+	 * @return the dialog
+	 */
+	public EStationDialog getDialog() {
+		return this.dialog;
+	}
+	
+	/**
+	 * convert the device bytes into raw values, no calculation will take place here, see translateValue reverseTranslateValue
+	 * inactive or to be calculated data point are filled with 0 and needs to be handles after words
+	 * @@param points pointer to integer array to be filled with converted data
+	 * @@param dataBuffer byte arrax with the data to be converted
+	 */
+	public int[] converDataBytes(int[] points, byte[] dataBuffer) {		
+		
+		// 0=Spannung 1=Strom 2=Ladung 3=Leistung 4=Energie 5=VersorgungsSpg. 
+		points[0] = new Integer((((dataBuffer[35] & 0xFF)-0x80)*100 + ((dataBuffer[36] & 0xFF)-0x80))*10);  //35,36   feed-back voltage
+		points[1] = new Integer((((dataBuffer[33] & 0xFF)-0x80)*100 + ((dataBuffer[34] & 0xFF)-0x80))*10);  //33,34   feed-back current : 0=0.0A,900=9.00A
+		points[2] = new Integer((((dataBuffer[43] & 0xFF)-0x80)*100 + ((dataBuffer[44] & 0xFF)-0x80))*1000);  //43,44  cq_capa_dis;  : charged capacity
+		points[3] = 0;
+		points[4] = 0;
+		points[5] = new Integer((((dataBuffer[41] & 0xFF)-0x80)*100 + ((dataBuffer[42] & 0xFF)-0x80))*10);  //41,42  fd_in_12v;    : input voltage(00.00V 30.00V)
+		// 6=SpannungZelle1 7=SpannungZelle2 8=SpannungZelle3 9=SpannungZelle4 10=SpannungZelle5 11=SpannungZelle6
+		for (int i=0, j=0; i<points.length - 6; ++i, j+=2) {
+			//log_base.info("cell " + (i+1) + " points[" + (i+6) + "]  = new Integer((((dataBuffer[" + (j+45) + "] & 0xFF)-0x80)*100 + ((dataBuffer[" + (j+46)+ "] & 0xFF)-0x80))*10);");  //45,46 CELL_420v[1];
+			points[i+6]  = new Integer((((dataBuffer[j+45] & 0xFF)-0x80)*100 + ((dataBuffer[j+46] & 0xFF)-0x80))*10);  //45,46 CELL_420v[1];
+		}
+
+		return points;
+	}
+}
