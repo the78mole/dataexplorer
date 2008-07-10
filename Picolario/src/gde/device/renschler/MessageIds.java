dfbd3329457dfea84f6d63361d2c75c530046474
Winfried Bruegmann <winfried.bruegmann@web.de>
2008-07-10 14:24:47 +0000

    externalize strings - NLS support

---
+++ Picolario/src/gde/device/renschler/MessageIds.java
@@ -0,0 +1,47 @@
+/************************************************************************************** 
+  	This file is part of OpenSerialDataExplorer.
+
+		OpenSerialDataExplorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenSerialDataExplorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenSerialDataExplorer.  If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************************/
+package osde.device.renschler;
+
+/** 
+ * @author Winfried Brügmann 
+ * Do not edit, MessageIds are generated from messages.properties 
+ */ 
+public class MessageIds { 
+
+	public final static String	OSDE_MSGE1200 = "OSDE_MSGE1200";
+	public final static String	OSDE_MSGT1200 = "OSDE_MSGT1200";
+	public final static String	OSDE_MSGT1201 = "OSDE_MSGT1201";
+	public final static String	OSDE_MSGT1202 = "OSDE_MSGT1202";
+	public final static String	OSDE_MSGT1203 = "OSDE_MSGT1203";
+	public final static String	OSDE_MSGT1204 = "OSDE_MSGT1204";
+	public final static String	OSDE_MSGT1205 = "OSDE_MSGT1205";
+	public final static String	OSDE_MSGT1206 = "OSDE_MSGT1206";
+	public final static String	OSDE_MSGT1207 = "OSDE_MSGT1207";
+	public final static String	OSDE_MSGT1208 = "OSDE_MSGT1208";
+	public final static String	OSDE_MSGT1209 = "OSDE_MSGT1209";
+	public final static String	OSDE_MSGT1210 = "OSDE_MSGT1210";
+	public final static String	OSDE_MSGT1211 = "OSDE_MSGT1211";
+	public final static String	OSDE_MSGT1212 = "OSDE_MSGT1212";
+	public final static String	OSDE_MSGT1213 = "OSDE_MSGT1213";
+	public final static String	OSDE_MSGT1214 = "OSDE_MSGT1214";
+	public final static String	OSDE_MSGT1215 = "OSDE_MSGT1215";
+	public final static String	OSDE_MSGT1216 = "OSDE_MSGT1216";
+	public final static String	OSDE_MSGT1217 = "OSDE_MSGT1217";
+	public final static String	OSDE_MSGT1218 = "OSDE_MSGT1218";
+	public final static String	OSDE_MSGT1219 = "OSDE_MSGT1219";
+
+ }
