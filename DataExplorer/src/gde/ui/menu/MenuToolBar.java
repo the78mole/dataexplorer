48f82ee00a98bc3a19689d31a6e193b705586680
Winfried Bruegmann <winfried.bruegmann@web.de>
2008-08-07 14:03:53 +0000

    modify menu tool bar size settings

--- DataExplorer/src/gde/ui/menu/MenuToolBar.java
+++ DataExplorer/src/gde/ui/menu/MenuToolBar.java
@@ -673,10 +673,10 @@ public class MenuToolBar {
 					this.recordSelectCombo.setLocation((recordSelectCompositeSize.x-this.recordSelectSize.x)/2, (recordSelectCompositeSize.y-this.recordSelectSize.y)/2);
 				}
 				this.dataBarComposite.pack();
-				int height = this.size.y+2;
-				this.size = this.dataBarComposite.getSize();
-				log.fine("pre dataBarComposite.size = " + this.size); //$NON-NLS-1$
-				this.dataBarComposite.setSize(this.size.x, height);
+				//int height = this.size.y+2;
+				//this.size = this.dataBarComposite.getSize();
+				//log.fine("pre dataBarComposite.size = " + this.size); //$NON-NLS-1$
+				//this.dataBarComposite.setSize(this.size.x, height);
 				this.size = this.dataBarComposite.getSize();
 				log.fine("post dataBarComposite.size = " + this.size); //$NON-NLS-1$
 			}
