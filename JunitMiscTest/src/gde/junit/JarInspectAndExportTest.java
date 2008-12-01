2adab4b89bc948dccce67e8e27d434e50ef11bb4
Winfried Bruegmann <winfried.bruegmann@web.de>
2008-12-01 07:16:32 +0000

    update build process as prerequisite for self-sufficient device plug-ins

---
+++ JunitMiscTest/src/gde/junit/JarInspectAndExportTest.java
@@ -0,0 +1,153 @@
+/**
+ * 
+ */
+package osde.junit;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+import junit.framework.TestCase;
+import osde.OSDE;
+import osde.messages.MessageIds;
+import osde.messages.Messages;
+import osde.utils.FileUtils;
+
+/**
+ * @author brueg
+ *
+ */
+public class JarInspectAndExportTest extends TestCase {
+	String	applHomePath	= null;
+	String	osname				= System.getProperty("os.name", "").toLowerCase();	//$NON-NLS-1$ //$NON-NLS-2$
+	Locale	locale;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.locale = new Locale(Locale.ENGLISH.getLanguage());
+
+		if (this.osname.startsWith("windows")) { //$NON-NLS-1$
+			this.applHomePath = (System.getenv("APPDATA") + OSDE.FILE_SEPARATOR_UNIX + "OpenSerialDataExplorer").replace("\\", "/"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		}
+		else if (this.osname.startsWith("linux")) { //$NON-NLS-1$
+			this.applHomePath = System.getProperty("user.home") + OSDE.FILE_SEPARATOR_UNIX + ".OpenSerialDataExplorer"; //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		else {
+			System.err.println(Messages.getString(MessageIds.OSDE_MSGW0001));
+			System.exit(-1);
+		}
+		System.out.println("applHomePath = " + this.applHomePath);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testListDeviceJars() {
+		HashMap<String, Exception> failures = new HashMap<String, Exception>();
+
+			File sourceDir = new File(FileUtils.getDeviceJarBasePath());
+			String[] files = sourceDir.list();
+			for (String jarName : files) {
+				System.out.println(jarName);
+			}
+
+		StringBuilder sb = new StringBuilder();
+		for (String key : failures.keySet()) {
+			sb.append(key).append(" - ").append(failures.get(key).getMessage()).append("\n");
+		}
+		if (failures.size() > 0) fail(sb.toString());
+	}
+
+	public void testListDeviceJarsManifest() {
+		HashMap<String, Exception> failures = new HashMap<String, Exception>();
+
+		String jarFileDir = FileUtils.getDeviceJarBasePath();
+		File sourceDir = new File(jarFileDir);
+		String[] files = sourceDir.list();
+		for (String fileName : files) {
+			if (!fileName.endsWith(".jar")) continue;
+			try {
+				JarFile jf = new JarFile(jarFileDir + "/" + fileName);
+				Manifest m = jf.getManifest();
+				System.out.println("\n" + fileName);
+				for (Object key : m.getMainAttributes().keySet()) {
+					System.out.println(key + ": " + m.getMainAttributes().get(key));
+				}
+			}
+			catch (IOException e) {
+				failures.put(e.getMessage(), e);
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (String key : failures.keySet()) {
+			sb.append(key).append(" - ").append(failures.get(key).getMessage()).append("\n");
+		}
+		if (failures.size() > 0) fail(sb.toString());
+	}
+
+	public void testExtractDevicePictures() {
+		HashMap<String, Exception> failures = new HashMap<String, Exception>();
+
+		String jarFileDir = FileUtils.getDeviceJarBasePath();
+		File sourceDir = new File(jarFileDir);
+		String[] files = sourceDir.list();
+		try {
+			for (String jarFileName : files) {
+				if (!jarFileName.endsWith(".jar")) continue;
+				JarFile jarFile = new JarFile(jarFileDir + "/" + jarFileName);
+				String[] plugins = FileUtils.getDeviceJarServicesNames(jarFile);
+				for (String plugin : plugins) {
+					String targetDirectory = System.getProperty("java.io.tmpdir");
+					String sourceFileName = "resource/" + plugin + ".jpg";
+					FileUtils.extractFile(jarFile, sourceFileName, targetDirectory);
+				}
+			}
+		}
+		catch (IOException e) {
+			failures.put(e.getMessage(), e);
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (String key : failures.keySet()) {
+			sb.append(key).append(" - ").append(failures.get(key).getMessage()).append("\n");
+		}
+		if (failures.size() > 0) fail(sb.toString());
+	}
+
+	public void testExtractDeviceProperties() {
+		HashMap<String, Exception> failures = new HashMap<String, Exception>();
+
+		String jarFileDir = FileUtils.getDeviceJarBasePath();
+		File sourceDir = new File(jarFileDir);
+		String[] files = sourceDir.list();
+		try {
+			for (String jarFileName : files) {
+				if (!jarFileName.endsWith(".jar")) continue;
+				JarFile jarFile = new JarFile(jarFileDir + "/" + jarFileName);
+				String[] plugins = FileUtils.getDeviceJarServicesNames(jarFile);
+				for (String plugin : plugins) {
+					String targetDirectory = System.getProperty("java.io.tmpdir");
+					String sourceFileName = "resource/en/" + plugin + ".xml";
+					FileUtils.extractFile(jarFile, sourceFileName, targetDirectory);
+				}
+			}
+		}
+		catch (IOException e) {
+			failures.put(e.getMessage(), e);
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (String key : failures.keySet()) {
+			sb.append(key).append(" - ").append(failures.get(key).getMessage()).append("\n");
+		}
+		if (failures.size() > 0) fail(sb.toString());
+	}
+
+}
