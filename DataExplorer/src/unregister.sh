442a4199354f208dd506f4fd47b6285e5d85e52e
Winfried Bruegmann <winfried.bruegmann@web.de>
2009-05-09 09:40:24 +0000

    operating system shell integration

--- DataExplorer/src/unregister.sh
+++ DataExplorer/src/unregister.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ***** script to register OpenSerialDataExplorer mime type ** 22 Apr 2009 WB *****
 
-#xdg-mime default /opt/OpenSerialDataExplorer/OpenSerialDataExplorer.desktop ""
+#xdg-mime default OpenSerialDataExplorer.desktop ""
 #sudo rm -f /usr/bin/OpenSerialDataExplorer
-xdg-desktop-menu uninstall --novendor /opt/OpenSerialDataExplorer/OpenSerialDataExplorer.directory /opt/OpenSerialDataExplorer/OpenSerialDataExplorer.desktop
-xdg-icon-resource uninstall --context mimetypes --size 48 /opt/OpenSerialDataExplorer/OpenSerialDataExplorer.xpm application-x-OpenSerialData
-xdg-mime uninstall --novendor /opt/OpenSerialDataExplorer/OpenSerialData-mime.xml
+xdg-desktop-menu uninstall --novendor /tmp/OpenSerialDataExplorer.directory OpenSerialDataExplorer.desktop
+xdg-icon-resource uninstall --context mimetypes --size 48 OpenSerialDataExplorer.xpm application-x-OpenSerialData
+xdg-mime uninstall --novendor OpenSerialData-mime.xml
