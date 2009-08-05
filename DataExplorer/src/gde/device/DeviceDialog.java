7825358acd8abb40beefeb35b7ef1ddb39cb0099
Winfried Bruegmann <winfried.bruegmann@web.de>
2009-08-05 11:13:07 +0000

    misc update

--- DataExplorer/src/gde/device/DeviceDialog.java
+++ DataExplorer/src/gde/device/DeviceDialog.java
@@ -127,7 +127,7 @@ public abstract class DeviceDialog extends Dialog {
 	 * @return the dialogShell
 	 */
 	public Shell getDialogShell() {
-		return this.dialogShell != null ? this.dialogShell : this.application.getShell();
+		return this.dialogShell != null && !this.dialogShell.isDisposed() ? this.dialogShell : this.application.getShell();
 	}
 
 	/**
