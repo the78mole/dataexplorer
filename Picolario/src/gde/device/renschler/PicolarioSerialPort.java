6cfc960f797584e3e74a033c0ae8afa78ae72d8d
Winfried Bruegmann <winfried.bruegmann@web.de>
2010-01-17 17:30:55 +0000

    add Level.TIME to enable time measurements without other logging

--- Picolario/src/gde/device/renschler/PicolarioSerialPort.java
+++ Picolario/src/gde/device/renschler/PicolarioSerialPort.java
@@ -20,7 +20,7 @@ import gnu.io.NoSuchPortException;
 
 import java.io.IOException;
 import java.util.Vector;
-import java.util.logging.Level;
+import osde.log.Level;
 import java.util.logging.Logger;
 
 import osde.device.DeviceConfiguration;
