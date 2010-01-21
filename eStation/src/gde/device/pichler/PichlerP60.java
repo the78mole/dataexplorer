be9203a70345c5201bfc99d6efb720995c44d0d2
Winfried Bruegmann <winfried.bruegmann@web.de>
2010-01-21 21:33:55 +0000

    re-org  projects and dependent

---
+++ eStation/src/gde/device/pichler/PichlerP60.java
@@ -0,0 +1,48 @@
+/**************************************************************************************
+  	This file is part of OpenSerialDataExplorer.
+
+    OpenSerialDataExplorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenSerialDataExplorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenSerialDataExplorer.  If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************************/
+package osde.device.pichler;
+
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBException;
+
+import osde.device.DeviceConfiguration;
+import osde.device.bantam.eStationBC6;
+
+/**
+ * Pichler P60 device class which is 100% eStation BC6
+ * @author Winfried Brügmann
+ */
+public class PichlerP60 extends eStationBC6 {
+
+	/**
+	 * @param deviceProperties
+	 * @throws FileNotFoundException
+	 * @throws JAXBException
+	 */
+	public PichlerP60(String deviceProperties) throws FileNotFoundException, JAXBException {
+		super(deviceProperties);
+	}
+
+	/**
+	 * @param deviceConfig
+	 */
+	public PichlerP60(DeviceConfiguration deviceConfig) {
+		super(deviceConfig);
+	}
+
+}
