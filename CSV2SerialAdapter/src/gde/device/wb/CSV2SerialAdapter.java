25c00be1f87d3282b2193936f79da4c9438d7cd0
Winfried Bruegmann <winfried.bruegmann@web.de>
2010-01-27 11:51:26 +0000

    update device properties XML style sheet to V08 to reflect OF required changes

--- CSV2SerialAdapter/src/gde/device/wb/CSV2SerialAdapter.java
+++ CSV2SerialAdapter/src/gde/device/wb/CSV2SerialAdapter.java
@@ -131,7 +131,7 @@ public class CSV2SerialAdapter extends DeviceConfiguration implements IDevice {
 	 */
 	public synchronized void addConvertedLovDataBufferAsRawDataPoints(RecordSet recordSet, byte[] dataBuffer, int recordDataSize, boolean doUpdateProgressBar) throws DataInconsitsentException {
 		// prepare the serial CSV data parser
-		DataParser data = new  DataParser(this.getDataBlockTimeUnitFactor(), this.getDataBlockSeparator().value(), this.getDataBlockCheckSumType(), this.getDataBlockSize());
+		DataParser data = new  DataParser(this.getDataBlockTimeUnitFactor(), this.getDataBlockLeader(), this.getDataBlockSeparator().value(), this.getDataBlockCheckSumType(), this.getDataBlockSize());
 		int[] startLength = new int[] {0,0};
 		byte[] lineBuffer = null;
 		String sThreadId = String.format("%06d", Thread.currentThread().getId()); //$NON-NLS-1$
