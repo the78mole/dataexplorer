b955dc69ef17a4e64797fbe6b87014bdebea7b5f
Winfried Bruegmann <winfried.bruegmann@web.de>
2010-06-02 17:04:10 +0000

    GNU adaptions

--- Simulator/src/gde/device/wb/SimulatorSerialPort.java
+++ Simulator/src/gde/device/wb/SimulatorSerialPort.java
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
     
-    Copyright (c) 2008 - 2010 Winfried Bruegmann
+    Copyright (c) 2008,2009,2010 Winfried Bruegmann
 ****************************************************************************************/
 package gde.device.wb;
 
