0a3ddb3809eee7a4f2aa473b6d9c7be323d73e89
Winfried Bruegmann <winfried.bruegmann@web.de>
2010-10-10 16:27:27 +0000

    misc update

--- Simulator/src/gde/device/wb/Simulator.java
+++ Simulator/src/gde/device/wb/Simulator.java
@@ -37,8 +37,6 @@ import java.util.logging.Logger;
 
 import javax.xml.bind.JAXBException;
 
-import org.eclipse.swt.custom.CTabItem;
-
 /**
  * Sample and test device implementation, scale adjustment, ...
  * @author Winfried Brügmann
@@ -247,6 +245,13 @@ public class Simulator extends DeviceConfiguration implements IDevice {
 		//do not forget to make record displayable -> record.setDisplayable(true);
 		log.log(Level.FINE, "working with " + recordSet.getName()); //$NON-NLS-1$
 	}
+	
+	/**
+	 * method toggle open close serial port or start/stop gathering data from device
+	 */
+	public void openCloseSerialPort() {
+		// no serial port is used
+	}
 
 	/**
 	 * @return the dialog
@@ -269,36 +274,5 @@ public class Simulator extends DeviceConfiguration implements IDevice {
 	 */
 	public String[] getUsedPropertyKeys() {
 		return new String[] {IDevice.OFFSET, IDevice.FACTOR, IDevice.REDUCTION};
-	}
-	
-	/**
-	 * method toggle open close serial port or start/stop gathering data from device
-	 */
-	public void openCloseSerialPort() {
-		// no serial port is used
-	}
-	
-	/**
-	 * set the measurement ordinal of the values displayed in cell voltage window underneath the cell voltage bars
-	 * set value of -1 to suppress this measurement
-	 */
-	public int[] getCellVoltageOrdinals() {
-		return new int[] {-1, -1};
-	}
-	
-	/**
-	 * query if an utility graphics window tab is requested
-	 */
-	public boolean isUtilityGraphicsRequested() {
-		return false;
-	}
-	
-	/**
-	 * This function allows to register a custom CTabItem to the main application tab folder to display device 
-	 * specific curve calculated from point combinations or other specific dialog
-	 * As default the function should return null which stands for no device custom tab item.  
-	 */
-	public CTabItem getUtilityDeviceTabItem() {
-		return null;
 	}
 }
