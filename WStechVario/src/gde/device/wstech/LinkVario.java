b8625cf5a7537afaeffb838f5414e1a50743731b
Winfried Bruegmann <winfried.bruegmann@web.de>
2010-11-07 11:08:28 +0000

    misc update

--- WStechVario/src/gde/device/wstech/LinkVario.java
+++ WStechVario/src/gde/device/wstech/LinkVario.java
@@ -18,24 +18,18 @@
 ****************************************************************************************/
 package gde.device.wstech;
 
+import gde.config.Settings;
+import gde.device.DeviceConfiguration;
+import gde.messages.Messages;
+import gde.serial.DeviceSerialPort;
+import gde.ui.DataExplorer;
+
 import java.io.FileNotFoundException;
 
 import javax.xml.bind.JAXBException;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabItem;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.widgets.MenuItem;
-
-import gde.config.Settings;
-import gde.device.DeviceConfiguration;
-import gde.io.FileHandler;
-import gde.log.Level;
-import gde.messages.Messages;
-import gde.serial.DeviceSerialPort;
-import gde.ui.DataExplorer;
 
 /**
  * Class to implement WSTech DataVario device properties extending the CSV2SerialAdapter class
@@ -88,75 +82,5 @@ public class LinkVario extends DataVario {
 	 */
 	public CTabItem getUtilityDeviceTabItem() {
 		return new VarioToolTabItem(this.application.getTabFolder(), SWT.NONE, this.application.getTabFolder().getItemCount(), this, false);
-	}
-	
-	
-	/**
-	 * update the file menu by adding two new entries to export KML/GPX files
-	 * @param exportMenue
-	 */
-	public void updateFileMenu(Menu exportMenue) {
-		MenuItem											convertKLM3DRelativeItem;
-		MenuItem											convertKLM3DAbsoluteItem;
-		MenuItem											convert2GPXRelativeItem;
-		MenuItem											convert2GPXAbsoluteItem;
-		
-		if (exportMenue.getItem(exportMenue.getItemCount() - 1).getText().equals(Messages.getString(gde.messages.MessageIds.GDE_MSGT0018))) {
-			new MenuItem(exportMenue, SWT.SEPARATOR);
-
-			convertKLM3DRelativeItem = new MenuItem(exportMenue, SWT.PUSH);
-			convertKLM3DRelativeItem.setText(Messages.getString(MessageIds.GDE_MSGT1895));
-			convertKLM3DRelativeItem.addListener(SWT.Selection, new Listener() {
-				public void handleEvent(Event e) {
-					ContextMenu.log.log(Level.FINEST, "convertKLM3DRelativeItem action performed! " + e); //$NON-NLS-1$
-					export2KML3D(DataVario.HEIGHT_RELATIVE);
-				}
-			});
-
-			convertKLM3DAbsoluteItem = new MenuItem(exportMenue, SWT.PUSH);
-			convertKLM3DAbsoluteItem.setText(Messages.getString(MessageIds.GDE_MSGT1896));
-			convertKLM3DAbsoluteItem.addListener(SWT.Selection, new Listener() {
-				public void handleEvent(Event e) {
-					ContextMenu.log.log(Level.FINEST, "convertKLM3DAbsoluteItem action performed! " + e); //$NON-NLS-1$
-					export2KML3D(DataVario.HEIGHT_ABSOLUTE);
-				}
-			});
-
-			convert2GPXRelativeItem = new MenuItem(exportMenue, SWT.PUSH);
-			convert2GPXRelativeItem.setText(Messages.getString(MessageIds.GDE_MSGT1897));
-			convert2GPXRelativeItem.addListener(SWT.Selection, new Listener() {
-				public void handleEvent(Event e) {
-					ContextMenu.log.log(Level.FINEST, "convert2GPXRelativeItem action performed! " + e); //$NON-NLS-1$
-					export2GPX(DataVario.HEIGHT_RELATIVE);
-				}
-			});
-
-			convert2GPXAbsoluteItem = new MenuItem(exportMenue, SWT.PUSH);
-			convert2GPXAbsoluteItem.setText(Messages.getString(MessageIds.GDE_MSGT1898));
-			convert2GPXAbsoluteItem.addListener(SWT.Selection, new Listener() {
-				public void handleEvent(Event e) {
-					ContextMenu.log.log(Level.FINEST, "convert2GPXAbsoluteItem action performed! " + e); //$NON-NLS-1$
-					export2GPX(DataVario.HEIGHT_ABSOLUTE);
-				}
-			});
-		}
-	}
-
-	/**
-	 * exports the actual displayed data set to KML file format
-	 * @param type DataVario.HEIGHT_RELATIVE | DataVario.HEIGHT_ABSOLUTE
-	 */
-	public void export2KML3D(int type) {
-		//ordinalLongitude, ordinalLatitude, ordinalGPSHeight, inRelative
-		new FileHandler().exportFileKML("export KML file with GPS height", 7, 8, 9, 10, type == DataVario.HEIGHT_RELATIVE);
-	}
-	
-	/**
-	 * exports the actual displayed data set to GPX file format
-	 * @param type DataVario.HEIGHT_RELATIVE | DataVario.HEIGHT_ABSOLUTE
-	 */
-	public void export2GPX(int type) {
-		//ordinalLongitude, ordinalLatitude, ordinalGPSHeight, ordinalVelocity, ordinalHeight, inRelative
-		new FileHandler().exportFileGPX("export GPX file", 7, 8, 9, 10, 1, type == DataVario.HEIGHT_RELATIVE);
-	}
+	}	
 }
