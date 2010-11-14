1fcfa41807aa71cdace70a6476e9d7c33d07bc07
Winfried Bruegmann <winfried.bruegmann@web.de>
2010-11-14 19:20:52 +0000

    misc update

--- Picolario/src/gde/device/renschler/Picolario.java
+++ Picolario/src/gde/device/renschler/Picolario.java
@@ -365,7 +365,7 @@ public class Picolario extends DeviceConfiguration implements IDevice {
 	 * it makes less sense to display voltage and current curves, if only height has measurement data
 	 * at least an update of the graphics window should be included at the end of this method
 	 */
-	public void updateVisibilityStatus(RecordSet recordSet) {
+	public void updateVisibilityStatus(RecordSet recordSet, boolean includeReasonableDataCheck) {
 		recordSet.setAllVisibleAndDisplayable();
 	}
 
