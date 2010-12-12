6901c3b04613c9dbc760975fc9e275685937caf3
Winfried Bruegmann <winfried.bruegmann@web.de>
2010-12-12 15:27:17 +0000

    update IDevice interface to reflect IDeviceCommPort

--- LiPoWatch/src/gde/device/smmodellbau/LiPoWatch.java
+++ LiPoWatch/src/gde/device/smmodellbau/LiPoWatch.java
@@ -439,7 +439,7 @@ public class LiPoWatch extends DeviceConfiguration implements IDevice {
 	/**
 	 * method toggle open close serial port or start/stop gathering data from device
 	 */
-	public void openCloseSerialPort() {
+	public void open_closeCommPort() {
 		if (this.serialPort != null) {
 			if (!this.serialPort.isConnected()) {
 				try {
