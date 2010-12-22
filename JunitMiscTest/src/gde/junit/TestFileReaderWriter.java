31d8b9e08c31eaaf92a0feb2aca75f55b0584c6b
Winfried Bruegmann <winfried.bruegmann@web.de>
2010-12-22 16:20:27 +0000

    add NMEA files to junit tests

--- JunitMiscTest/src/gde/junit/TestFileReaderWriter.java
+++ JunitMiscTest/src/gde/junit/TestFileReaderWriter.java
@@ -27,6 +27,7 @@ import gde.exception.NotSupportedException;
 import gde.io.CSVReaderWriter;
 import gde.io.CSVSerialDataReaderWriter;
 import gde.io.LogViewReader;
+import gde.io.NMEAReaderWriter;
 import gde.io.OsdReaderWriter;
 import gde.utils.FileUtils;
 import gde.utils.OperatingSystemHelper;
@@ -51,8 +52,8 @@ public class TestFileReaderWriter extends TestSuperClass {
 	}
 
 	/**
-	 * test reading LOV files from LogView application directory and writes OSD files to %TEMP%\Write_1_OSD
-	 * all consitent files must red without failures
+	 * test reading CSV files from device directory and writes OSD files to %TEMP%\Write_1_OSD
+	 * all consistent files must red without failures
 	 */
 	public final void testCsvReaderOsdWriter() {
 		HashMap<String, Exception> failures = new HashMap<String, Exception>();
@@ -137,6 +138,79 @@ public class TestFileReaderWriter extends TestSuperClass {
 						new File(tmpDir1).mkdirs();
 						String absolutFilePath = tmpDir1 + file.getName();
 						absolutFilePath = absolutFilePath.substring(0, absolutFilePath.length() - 4) + "_cvs.osd";
+						System.out.println("writing as   : " + absolutFilePath);
+						OsdReaderWriter.write(absolutFilePath, this.channels.getActiveChannel(), GDE.DATA_EXPLORER_FILE_VERSION_INT);
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+						failures.put(file.getAbsolutePath(), e);
+					}
+				}
+			}
+
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (String key : failures.keySet()) {
+			sb.append(key).append(" - ").append(failures.get(key).getMessage()).append("\n");
+		}
+		if (failures.size() > 0) fail(sb.toString());
+	}
+
+	/**
+	 * test reading NMEA files from device directory and writes OSD files to %TEMP%\Write_1_OSD
+	 * all consistent files must red without failures
+	 */
+	public final void testNmeaReaderOsdWriter() {
+		HashMap<String, Exception> failures = new HashMap<String, Exception>();
+
+		this.setDataPath(); //set the dataPath variable
+
+		try {
+			List<File> files = FileUtils.getFileListing(this.dataPath);
+
+			for (File file : files) {
+				if (file.getAbsolutePath().toLowerCase().endsWith(".nmea")) {
+					System.out.println("working with : " + file);
+					
+					try {
+						//System.out.println("file.getPath() = " + file.getPath());
+						String deviceName = file.getPath().substring(0, file.getPath().lastIndexOf(GDE.FILE_SEPARATOR));
+						deviceName = deviceName.substring(1+deviceName.lastIndexOf(GDE.FILE_SEPARATOR));
+						//System.out.println("deviceName = " + deviceName);
+						DeviceConfiguration deviceConfig = this.deviceConfigurations.get(deviceName);
+						if (deviceConfig == null) throw new NotSupportedException("device = " + deviceName + " is not supported or in list of active devices");
+
+						// GPS-Logger and similar file
+						IDevice device = this.getInstanceOfDevice(deviceConfig);
+						this.application.setActiveDeviceWoutUI(device);
+
+						setupDataChannels(device);
+
+						this.channels.setActiveChannelNumber(1);
+						Channel activeChannel = this.channels.getActiveChannel();
+						activeChannel.setFileName(file.getAbsolutePath());
+						activeChannel.setFileDescription(StringHelper.getDateAndTime() + " - imported from NMEA file");
+						activeChannel.setSaved(true);
+
+						NMEAReaderWriter.read(file.getAbsolutePath(), device, "RecordSet", 1);
+						RecordSet recordSet = activeChannel.getActiveRecordSet();
+
+						if (recordSet != null) {
+							activeChannel.setActiveRecordSet(recordSet);
+							activeChannel.applyTemplate(recordSet.getName(), true);
+							//device.makeInActiveDisplayable(recordSet);
+							drawCurves(recordSet, 1024, 768);
+						}
+
+						String tmpDir1 = this.tmpDir + "Write_1_OSD" + GDE.FILE_SEPARATOR;
+						new File(tmpDir1).mkdirs();
+						String absolutFilePath = tmpDir1 + file.getName();
+						absolutFilePath = absolutFilePath.substring(0, absolutFilePath.length() - 4) + "_nmea.osd";
 						System.out.println("writing as   : " + absolutFilePath);
 						OsdReaderWriter.write(absolutFilePath, this.channels.getActiveChannel(), GDE.DATA_EXPLORER_FILE_VERSION_INT);
 					}
@@ -392,6 +466,72 @@ public class TestFileReaderWriter extends TestSuperClass {
 
 			for (File file : files) {
 				if (file.getAbsolutePath().toLowerCase().endsWith(".osd")) {
+					System.out.println("working with : " + file);
+					try {
+						HashMap<String, String> fileHeader = OsdReaderWriter.getHeader(file.getAbsolutePath());
+						String fileDeviceName = fileHeader.get(GDE.DEVICE_NAME);
+						DeviceConfiguration deviceConfig = this.deviceConfigurations.get(fileDeviceName);
+						IDevice device = this.getInstanceOfDevice(deviceConfig);
+						this.application.setActiveDeviceWoutUI(device);
+
+						setupDataChannels(device);
+
+						OsdReaderWriter.read(file.getAbsolutePath());
+
+						Channel activeChannel = this.channels.getActiveChannel();
+						activeChannel.setFileName(file.getAbsolutePath());
+						activeChannel.setFileDescription(fileHeader.get(GDE.FILE_COMMENT));
+						activeChannel.setSaved(true);
+						activeChannel.checkAndLoadData(); //perform this operation triggered by drawCurves
+
+						for (String recordSetName : activeChannel.getRecordSetNames()) {
+							RecordSet recordSet = activeChannel.get(recordSetName);
+							if (recordSet != null) {
+								activeChannel.setActiveRecordSet(recordSet);
+								//device.makeInActiveDisplayable(recordSet);
+								drawCurves(recordSet, 1024, 768);
+							}
+						}
+
+						String tmpDir2 = this.tmpDir + "Write_2_OSD" + GDE.FILE_SEPARATOR;
+						new File(tmpDir2).mkdirs();
+						String absolutFilePath = tmpDir2 + file.getName();
+						System.out.println("writing as   : " + absolutFilePath);
+						OsdReaderWriter.write(absolutFilePath, this.channels.getActiveChannel(), GDE.DATA_EXPLORER_FILE_VERSION_INT);
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+						failures.put(file.getAbsolutePath(), e);
+					}
+				}
+			}
+
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (String key : failures.keySet()) {
+			sb.append(key).append(" - ").append(failures.get(key).getMessage()).append("\n");
+		}
+		if (failures.size() > 0) fail(sb.toString());
+	}
+
+	/**
+	 * test reading NMEA files %TEMP%\Write_1_OSD and writes OSD files to %TEMP%\Write_2_OSD
+	 * all files must identical except time stamp
+	 */
+	public final void testCSVAdapterWriterOsd() {
+		HashMap<String, Exception> failures = new HashMap<String, Exception>();
+
+		try {
+			String tmpDir1 = this.tmpDir + GDE.FILE_SEPARATOR + "Write_1_OSD" + GDE.FILE_SEPARATOR;
+			List<File> files = FileUtils.getFileListing(new File(tmpDir1));
+
+			for (File file : files) {
+				if (file.getAbsolutePath().toLowerCase().endsWith(".csv") && file.getName().toLowerCase().startsWith("LOG")) {
 					System.out.println("working with : " + file);
 					try {
 						HashMap<String, String> fileHeader = OsdReaderWriter.getHeader(file.getAbsolutePath());
