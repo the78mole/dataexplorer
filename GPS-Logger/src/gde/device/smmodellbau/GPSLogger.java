a7e5a0b0521c64ce3b04c4c1d8ff056dc892dd41
Winfried Bruegmann <winfried.bruegmann@web.de>
2011-01-29 12:29:53 +0000

    fix file import problem upper case extension

--- GPS-Logger/src/gde/device/smmodellbau/GPSLogger.java
+++ GPS-Logger/src/gde/device/smmodellbau/GPSLogger.java
@@ -480,7 +480,7 @@ public class GPSLogger extends DeviceConfiguration implements IDevice {
 			public void run() {
 				for (String tmpFileName : fd.getFileNames()) {
 					String selectedImportFile = fd.getFilterPath() + GDE.FILE_SEPARATOR_UNIX + tmpFileName;
-					if (!selectedImportFile.endsWith(GDE.FILE_ENDING_DOT_NMEA)) {
+					if (!selectedImportFile.toLowerCase().endsWith(GDE.FILE_ENDING_DOT_NMEA)) {
 						if (selectedImportFile.contains(GDE.STRING_DOT)) {
 							selectedImportFile = selectedImportFile.substring(0, selectedImportFile.indexOf(GDE.STRING_DOT));
 						}
