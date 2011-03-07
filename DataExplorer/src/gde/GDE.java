ed745278e7ca70c8f31619b8a3ea29434e28bcbd
Winfried Bruegmann <winfried.bruegmann@web.de>
2011-03-07 19:36:30 +0000

    misc update

--- DataExplorer/src/gde/GDE.java
+++ DataExplorer/src/gde/GDE.java
@@ -76,7 +76,7 @@ public class GDE {
 
 
 	// ****** begin global constants section *******
-	public static final String		VERSION													= "Version 2.73";																																																						//$NON-NLS-1$
+	public static final String		VERSION													= "Version 2.74";																																																						//$NON-NLS-1$
 	public static final String		NAME_SHORT											= "GDE";																																																											//$NON-NLS-1$
 	public static final String		NAME_LONG												= "DataExplorer";																																																						//$NON-NLS-1$
 	public final static String		DEVICE_PROPERTIES_XSD_VERSION		= "_V12";																																																										//$NON-NLS-1$
