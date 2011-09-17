cafe9a8e02e1a0100be2a4900436e74dada464dd
Winfried Bruegmann <winfried.bruegmann@web.de>
2011-09-17 17:09:57 +0000

    modify code updating default data directory from plugged storage media

--- NMEA-Adapter/src/gde/device/nmea/NMEAAdapter.java
+++ NMEA-Adapter/src/gde/device/nmea/NMEAAdapter.java
@@ -485,6 +485,9 @@ public class NMEAAdapter extends DeviceConfiguration implements IDevice {
 		}
 		final FileDialog fd = this.application.openFileOpenDialog(Messages.getString(MessageIds.GDE_MSGT2100), new String[] { this.getDeviceConfiguration().getDataBlockPreferredFileExtention(),
 				GDE.FILE_ENDING_STAR_STAR }, searchDirectory, null, SWT.MULTI);
+		
+		this.getDeviceConfiguration().setDataBlockPreferredDataLocation(fd.getFilterPath());
+
 		Thread reader = new Thread("reader"){
 			@Override
 			public void run() {
