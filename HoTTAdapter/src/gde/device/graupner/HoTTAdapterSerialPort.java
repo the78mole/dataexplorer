8d5dcdfdbc2305e41e69c8abcd2a971d32b20edf
Winfried Bruegmann <winfried.bruegmann@web.de>
2011-12-28 14:51:14 +0000

    enable detection of missed query sensor writes

--- HoTTAdapter/src/gde/device/graupner/HoTTAdapterSerialPort.java
+++ HoTTAdapter/src/gde/device/graupner/HoTTAdapterSerialPort.java
@@ -20,8 +20,12 @@ package gde.device.graupner;
 
 import gde.comm.DeviceCommPort;
 import gde.device.DeviceConfiguration;
+import gde.exception.FailedQueryException;
 import gde.exception.SerialPortException;
 import gde.exception.TimeOutException;
+import gde.log.Level;
+import gde.messages.MessageIds;
+import gde.messages.Messages;
 import gde.ui.DataExplorer;
 import gde.utils.StringHelper;
 import gde.utils.WaitTimer;
@@ -36,7 +40,7 @@ import java.util.logging.Logger;
 public class HoTTAdapterSerialPort extends DeviceCommPort {
 	final static String		$CLASS_NAME									= HoTTAdapterSerialPort.class.getName();
 	final static Logger		log													= Logger.getLogger(HoTTAdapterSerialPort.$CLASS_NAME);
-	static final int			READ_TIMEOUT_MS							= 1000;
+	static final int			READ_TIMEOUT_MS							= 400;
 
 	//HoTT sensor bytes legacy
 	final static byte[]		QUERY_SENSOR_DATA						= new byte[] { (byte) 0x80 };
@@ -58,6 +62,7 @@ public class HoTTAdapterSerialPort extends DeviceCommPort {
 	byte[]								SENSOR_TYPE									= new byte[] { HoTTAdapter.SENSOR_TYPE_RECEIVER_19200 };
 	byte[]								QUERY_SENSOR_TYPE;
 	final static int			xferErrorLimit							= 1000;
+	boolean								isQueryRetry								= false;
 
 	boolean								isInterruptedByUser					= false;
 	HoTTAdapter.Protocol	protocolType								= HoTTAdapter.Protocol.TYPE_19200_N;
@@ -106,7 +111,7 @@ public class HoTTAdapterSerialPort extends DeviceCommPort {
 			}
 
 			if (HoTTAdapterSerialPort.ANSWER[0] == this.SENSOR_TYPE[0]) {
-				this.read(this.ANSWER_DATA, HoTTAdapterSerialPort.READ_TIMEOUT_MS);
+				this.read(this.ANSWER_DATA, HoTTAdapterSerialPort.READ_TIMEOUT_MS, true);
 			}
 
 			if (checkBeginEndSignature && HoTTAdapter.IS_SLAVE_MODE) {
@@ -126,15 +131,22 @@ public class HoTTAdapterSerialPort extends DeviceCommPort {
 						"=====> data start or end does not match, number of errors = " + this.getXferErrors());
 				if (this.getXferErrors() > HoTTAdapterSerialPort.xferErrorLimit)
 					throw new SerialPortException("Number of tranfer error exceed the acceptable limit of " + HoTTAdapterSerialPort.xferErrorLimit);
-				WaitTimer.delay(200);
+				WaitTimer.delay(HoTTAdapter.QUERY_GAP_MS);
 				data = getData(true);
 			}
+			this.isQueryRetry = false;
 		}
-		catch (Exception e) {
-			if (!(e instanceof TimeOutException)) {
-				HoTTAdapterSerialPort.log.logp(java.util.logging.Level.SEVERE, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, e.getMessage(), e);
+		catch (FailedQueryException e) {
+			if (!this.isQueryRetry) {
+				this.isQueryRetry = true;
+				data = getData(true);
 			}
-			throw e;
+			else {
+				this.isQueryRetry = true;
+				TimeOutException te = new TimeOutException(Messages.getString(MessageIds.GDE_MSGE0011, new Object[] { this.ANSWER_DATA.length, HoTTAdapterSerialPort.READ_TIMEOUT_MS }));
+				log.log(Level.SEVERE, te.getMessage(), te);
+				throw te;
+			}
 		}
 		if (HoTTAdapterSerialPort.log.isLoggable(java.util.logging.Level.FINE)) {
 			HoTTAdapterSerialPort.log.logp(java.util.logging.Level.FINER, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, StringHelper.byte2FourDigitsIntegerString(data));
@@ -194,59 +206,68 @@ public class HoTTAdapterSerialPort extends DeviceCommPort {
 		byte[] answer = new byte[this.ANSWER_DATA.length];
 		byte[] data = new byte[this.DATA_LENGTH];
 
-		this.write(this.QUERY_SENSOR_TYPE);
-
-		this.read(answer, HoTTAdapterSerialPort.READ_TIMEOUT_MS);
-		data[0] = this.QUERY_SENSOR_TYPE[6];
-		System.arraycopy(answer, 0, data, 1, answer.length);
-
-		if (HoTTAdapterSerialPort.log.isLoggable(java.util.logging.Level.FINE)) {
-			HoTTAdapterSerialPort.log.logp(java.util.logging.Level.FINER, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, StringHelper.byte2FourDigitsIntegerString(data));
-			HoTTAdapterSerialPort.log.logp(java.util.logging.Level.FINE, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, StringHelper.byte2Hex2CharString(data, data.length));
-		}
-
-		if (!this.isInterruptedByUser && (answer[0] != 0x00 || answer[4] != 0x00 || answer[5] != 0x04 || answer[6] != 0x01 || (answer[answer.length - 3] < 0 && answer[answer.length - 3] > 100))) {
-			this.addXferError();
-			HoTTAdapterSerialPort.log
-					.logp(java.util.logging.Level.WARNING, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, "=====> transmission error occurred, number of errors = " + this.getXferErrors());
-			if (this.getXferErrors() > HoTTAdapterSerialPort.xferErrorLimit)
-				throw new SerialPortException("Number of transfer error exceed the acceptable limit of " + HoTTAdapterSerialPort.xferErrorLimit);
-			WaitTimer.delay(200);
-			data = getData(queryDBM);
-		}
-
-
-		//sensor type is receiver need to query DBM data in addition
-		if (queryDBM > 0 && this.QUERY_SENSOR_TYPE[6] == HoTTAdapterSerialPort.QUERY_SENSOR_DATA_RECEIVER[6]) {
-			WaitTimer.delay(HoTTAdapter.QUERY_GAP_MS);
-			++HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[1];
-			--HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[2];
-			this.write(HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM);
-			this.read(answerDBM, HoTTAdapterSerialPort.READ_TIMEOUT_MS);
-
+		try {
+			this.write(this.QUERY_SENSOR_TYPE);
+			this.read(answer, HoTTAdapterSerialPort.READ_TIMEOUT_MS, true);
+			data[0] = this.QUERY_SENSOR_TYPE[6];
+			System.arraycopy(answer, 0, data, 1, answer.length);
 			if (HoTTAdapterSerialPort.log.isLoggable(java.util.logging.Level.FINE)) {
-				HoTTAdapterSerialPort.log.logp(java.util.logging.Level.FINER, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, StringHelper.byte2FourDigitsIntegerString(answerDBM));
-				HoTTAdapterSerialPort.log.logp(java.util.logging.Level.FINE, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, StringHelper.byte2Hex2CharString(answerDBM, answerDBM.length));
+				HoTTAdapterSerialPort.log.logp(java.util.logging.Level.FINER, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, StringHelper.byte2FourDigitsIntegerString(data));
+				HoTTAdapterSerialPort.log.logp(java.util.logging.Level.FINE, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, StringHelper.byte2Hex2CharString(data, data.length));
 			}
-			if (HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[2] == 0xFE) {
-				HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[1] = 0;
-				HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[2] = (byte) 0xFF;
+			if (!this.isInterruptedByUser && (answer[0] != 0x00 || answer[4] != 0x00 || answer[5] != 0x04 || answer[6] != 0x01 || (answer[answer.length - 3] < 0 && answer[answer.length - 3] > 100))) {
+				this.addXferError();
+				HoTTAdapterSerialPort.log.logp(java.util.logging.Level.WARNING, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME,
+						"=====> transmission error occurred, number of errors = " + this.getXferErrors());
+				if (this.getXferErrors() > HoTTAdapterSerialPort.xferErrorLimit)
+					throw new SerialPortException("Number of transfer error exceed the acceptable limit of " + HoTTAdapterSerialPort.xferErrorLimit);
+				WaitTimer.delay(HoTTAdapter.QUERY_GAP_MS);
+				data = getData(queryDBM);
 			}
+			//sensor type is receiver need to query DBM data in addition
+			if (queryDBM > 0 && this.QUERY_SENSOR_TYPE[6] == HoTTAdapterSerialPort.QUERY_SENSOR_DATA_RECEIVER[6]) {
+				WaitTimer.delay(HoTTAdapter.QUERY_GAP_MS);
+				++HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[1];
+				--HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[2];
+				this.write(HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM);
+				this.read(answerDBM, HoTTAdapterSerialPort.READ_TIMEOUT_MS*2, true);
 
-			for (int i = 0; i < 75; i++) {
-				rxDBM += answerDBM[i + 157];
-				txDBM += answerDBM[i + 82];
-			}
-			data[4] = (byte) (rxDBM /= 75);
-			data[5] = (byte) (txDBM /= 75);
-			System.arraycopy(answer, 0, HoTTAdapterSerialPort.answerRx, 0, answer.length);
-		}
-		else {
-			data[3] = HoTTAdapterSerialPort.answerRx[16];
-			data[4] = HoTTAdapterSerialPort.answerRx[14];
-			data[5] = HoTTAdapterSerialPort.answerRx[9];
-		}
+				if (HoTTAdapterSerialPort.log.isLoggable(java.util.logging.Level.FINE)) {
+					HoTTAdapterSerialPort.log.logp(java.util.logging.Level.FINER, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, StringHelper.byte2FourDigitsIntegerString(answerDBM));
+					HoTTAdapterSerialPort.log.logp(java.util.logging.Level.FINE, HoTTAdapterSerialPort.$CLASS_NAME, $METHOD_NAME, StringHelper.byte2Hex2CharString(answerDBM, answerDBM.length));
+				}
+				if (HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[2] == 0xFE) {
+					HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[1] = 0;
+					HoTTAdapterSerialPort.QUERY_SENSOR_DATA_DBM[2] = (byte) 0xFF;
+				}
 
+				for (int i = 0; i < 75; i++) {
+					rxDBM += answerDBM[i + 157];
+					txDBM += answerDBM[i + 82];
+				}
+				data[4] = (byte) (rxDBM /= 75);
+				data[5] = (byte) (txDBM /= 75);
+				System.arraycopy(answer, 0, HoTTAdapterSerialPort.answerRx, 0, answer.length);
+			}
+			else {
+				data[3] = HoTTAdapterSerialPort.answerRx[16];
+				data[4] = HoTTAdapterSerialPort.answerRx[14];
+				data[5] = HoTTAdapterSerialPort.answerRx[9];
+			}
+			this.isQueryRetry = false;
+		}
+		catch (FailedQueryException e) {
+			if (!this.isQueryRetry) {
+				this.isQueryRetry = true;
+				data = getData(queryDBM);
+			}
+			else {
+				this.isQueryRetry = false;
+				TimeOutException te = new TimeOutException(Messages.getString(MessageIds.GDE_MSGE0011, new Object[] { this.ANSWER_DATA.length, HoTTAdapterSerialPort.READ_TIMEOUT_MS }));
+				log.log(Level.SEVERE, te.getMessage(), te);
+				throw te;
+			}
+		}
 		return data;
 	}
 
