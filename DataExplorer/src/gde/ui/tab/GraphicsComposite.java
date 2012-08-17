9b5021dad8ff3b1344ba39254ba525db41aba06e
Winfried Bruegmann <winfried.bruegmann@web.de>
2012-08-17 10:41:26 +0000

    MAC Mountain Lion fix graphics update problem

--- DataExplorer/src/gde/ui/tab/GraphicsComposite.java
+++ DataExplorer/src/gde/ui/tab/GraphicsComposite.java
@@ -327,7 +327,7 @@ public class GraphicsComposite extends Composite {
 			});
 			this.graphicCanvas.addPaintListener(new PaintListener() {
 				public void paintControl(PaintEvent evt) {
-					if (log.isLoggable(Level.FINER)) log.log(Level.FINER, "graphicCanvas.paintControl, event=" + evt); //$NON-NLS-1$
+					if (log.isLoggable(Level.OFF)) log.log(Level.OFF, "graphicCanvas.paintControl, event=" + evt); //$NON-NLS-1$
 					//System.out.println("width = " + GraphicsComposite.this.getSize().x);
 					try {
 						drawAreaPaintControl(evt);
@@ -641,16 +641,21 @@ public class GraphicsComposite extends Composite {
 		this.recordSetComment.redraw();
 		
 		if (GDE.IS_WINDOWS || (GDE.IS_MAC && !GDE.IS_MAC_MOUNTAIN_LION)) {
+			log.log(Level.OFF, "this.graphicCanvas.redraw(5,5,5,5,true); // image based - let OS handle the update");
 			Point size = this.graphicCanvas.getSize();
 			this.graphicCanvas.redraw(5,5,5,5,true); // image based - let OS handle the update
 			this.graphicCanvas.redraw(size.x-5,5,5,5,true);
 			this.graphicCanvas.redraw(5,size.y-5,5,5,true);
 			this.graphicCanvas.redraw(size.x-5,size.y-5,5,5,true);
 		}
-		else if (GDE.IS_MAC_MOUNTAIN_LION)
+		else if (GDE.IS_MAC_MOUNTAIN_LION) {
+			log.log(Level.OFF, "this.graphicCanvas.notifyListeners(SWT.Paint, new Event()); // GDE.IS_MAC_MOUNTAIN_LION");
 			this.graphicCanvas.notifyListeners(SWT.Paint, new Event());
-		else
+		}
+		else {
+			log.log(Level.OFF, "this.graphicCanvas.redraw(); // do full update");
 			this.graphicCanvas.redraw(); // do full update
+		}
 	}
 
 	public void notifySelected() {
