0dc4c796f1919747dc9daf65b00625ad2df4c0b9
Winfried Bruegmann <winfried.bruegmann@web.de>
2012-08-30 18:40:29 +0000

    misc update

--- DataExplorer/src/gde/ui/tab/GraphicsComposite.java
+++ DataExplorer/src/gde/ui/tab/GraphicsComposite.java
@@ -327,7 +327,7 @@ public class GraphicsComposite extends Composite {
 			});
 			this.graphicCanvas.addPaintListener(new PaintListener() {
 				public void paintControl(PaintEvent evt) {
-					if (log.isLoggable(Level.OFF)) log.log(Level.OFF, "graphicCanvas.paintControl, event=" + evt); //$NON-NLS-1$
+					//if (log.isLoggable(Level.OFF)) log.log(Level.OFF, "graphicCanvas.paintControl, event=" + evt); //$NON-NLS-1$
 					//System.out.println("width = " + GraphicsComposite.this.getSize().x);
 					try {
 						drawAreaPaintControl(evt);
@@ -641,7 +641,7 @@ public class GraphicsComposite extends Composite {
 		this.recordSetComment.redraw();
 		
 		if (GDE.IS_WINDOWS || (GDE.IS_MAC && !GDE.IS_MAC_MOUNTAIN_LION)) {
-			log.log(Level.OFF, "this.graphicCanvas.redraw(5,5,5,5,true); // image based - let OS handle the update");
+			//log.log(Level.OFF, "this.graphicCanvas.redraw(5,5,5,5,true); // image based - let OS handle the update");
 			Point size = this.graphicCanvas.getSize();
 			this.graphicCanvas.redraw(5,5,5,5,true); // image based - let OS handle the update
 			this.graphicCanvas.redraw(size.x-5,5,5,5,true);
@@ -649,11 +649,11 @@ public class GraphicsComposite extends Composite {
 			this.graphicCanvas.redraw(size.x-5,size.y-5,5,5,true);
 		}
 		else if (GDE.IS_MAC_MOUNTAIN_LION) {
-			log.log(Level.OFF, "this.graphicCanvas.notifyListeners(SWT.Paint, new Event()); // GDE.IS_MAC_MOUNTAIN_LION");
+			//log.log(Level.OFF, "this.graphicCanvas.notifyListeners(SWT.Paint, new Event()); // GDE.IS_MAC_MOUNTAIN_LION");
 			this.graphicCanvas.notifyListeners(SWT.Paint, new Event());
 		}
 		else {
-			log.log(Level.OFF, "this.graphicCanvas.redraw(); // do full update");
+			//log.log(Level.OFF, "this.graphicCanvas.redraw(); // do full update");
 			this.graphicCanvas.redraw(); // do full update
 		}
 	}
