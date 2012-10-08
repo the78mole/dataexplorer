b2e013d675a348232cd70d0c8c88900fe891e2fc
Winfried Bruegmann <winfried.bruegmann@web.de>
2012-10-08 15:55:48 +0000

    bug #37462 Add shortcuts like e.g. Ctrl+s to save

--- WStechVario/src/gde/device/wstech/DataVario.java
+++ WStechVario/src/gde/device/wstech/DataVario.java
@@ -723,7 +723,8 @@ public class DataVario  extends DeviceConfiguration implements IDevice {
 			new MenuItem(importMenue, SWT.SEPARATOR);
 
 			importDeviceLogItem = new MenuItem(importMenue, SWT.PUSH);
-			importDeviceLogItem.setText(Messages.getString(MessageIds.GDE_MSGT1877));
+			importDeviceLogItem.setText(Messages.getString(MessageIds.GDE_MSGT1877, GDE.MOD1));
+			importDeviceLogItem.setAccelerator(SWT.MOD1 + Messages.getAcceleratorChar(MessageIds.GDE_MSGT1877));
 			importDeviceLogItem.addListener(SWT.Selection, new Listener() {
 				public void handleEvent(Event e) {
 					log.log(java.util.logging.Level.FINEST, "importDeviceLogItem action performed! " + e); //$NON-NLS-1$
