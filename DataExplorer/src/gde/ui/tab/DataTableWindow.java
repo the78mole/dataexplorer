f2bf421c6ccc5850db774587b983c0819425a319
Winfried Bruegmann <winfried.bruegmann@web.de>
2012-10-16 09:26:01 +0000

    bug #37466 Cursor handling in Table view

--- DataExplorer/src/gde/ui/tab/DataTableWindow.java
+++ DataExplorer/src/gde/ui/tab/DataTableWindow.java
@@ -18,13 +18,19 @@
 ****************************************************************************************/
 package gde.ui.tab;
 
+import java.util.Vector;
 import java.util.logging.Logger;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.custom.TableCursor;
 import org.eclipse.swt.events.HelpEvent;
 import org.eclipse.swt.events.HelpListener;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
@@ -54,19 +60,22 @@ import gde.ui.menu.TabAreaContextMenu;
  * @author Winfried Brügmann
  */
 public class DataTableWindow extends CTabItem {
-	final static Logger					  log	= Logger.getLogger(DataTableWindow.class.getName());
+	final static Logger					log												= Logger.getLogger(DataTableWindow.class.getName());
 
-	final public static String		TABLE_TIME_STAMP_ABSOLUTE = "table_time_stamp_absolute";
+	final public static String	TABLE_TIME_STAMP_ABSOLUTE	= "table_time_stamp_absolute";
 
-	Table													dataTable;
-	TableColumn										timeColumn;
+	Table												dataTable;
+	TableColumn									timeColumn;
+	TableCursor									cursor;
+	Vector<Integer>							rowVector									= new Vector<Integer>(2);
+	Vector<Integer>							topindexVector						= new Vector<Integer>(2);
 
-	final DataExplorer						application;
-	final Channels								channels;
-	final CTabFolder							tabFolder;
-	final Menu										popupmenu;
-	final TabAreaContextMenu			contextMenu;
-	boolean												isAbsoluteDateTime = false;
+	final DataExplorer					application;
+	final Channels							channels;
+	final CTabFolder						tabFolder;
+	final Menu									popupmenu;
+	final TabAreaContextMenu		contextMenu;
+	boolean											isAbsoluteDateTime				= false;
 
 	public DataTableWindow(CTabFolder dataTab, int style, int position) {
 		super(dataTab, style, position);
@@ -76,26 +85,142 @@ public class DataTableWindow extends CTabItem {
 		this.channels = Channels.getInstance();
 		this.setFont(SWTResourceManager.getFont(this.application, 10, SWT.NORMAL));
 		this.setText(Messages.getString(MessageIds.GDE_MSGT0233));
-		
+
 		this.popupmenu = new Menu(this.application.getShell(), SWT.POP_UP);
 		this.contextMenu = new TabAreaContextMenu();
 	}
 
 	public void create() {
-		this.dataTable = new Table(this.tabFolder, SWT.VIRTUAL | SWT.BORDER);
+		this.dataTable = new Table(this.tabFolder, SWT.VIRTUAL | SWT.BORDER | SWT.MULTI | SWT.FULL_SELECTION);
 		this.setControl(this.dataTable);
 		this.dataTable.setLinesVisible(true);
 		this.dataTable.setHeaderVisible(true);
+
+		this.cursor = new TableCursor(this.dataTable, SWT.NONE);
+		this.cursor.addKeyListener(new KeyListener() {
+			public void keyReleased(KeyEvent event) {
+				if (log.isLoggable(Level.FINEST)) log.log(Level.FINEST, ("cursor.keyReleased, keycode: " + event.keyCode));
+
+				if (event.stateMask == SWT.MOD1) {
+					switch (event.keyCode) {
+					case SWT.HOME:
+						// Ctrl+Home: go to first row and first column
+						DataTableWindow.this.cursor.setSelection(0, 0);
+						break;
+					case SWT.END:
+						// Ctrl+End: go to last row and last column
+						DataTableWindow.this.cursor.setSelection(DataTableWindow.this.dataTable.getItemCount() - 1, DataTableWindow.this.dataTable.getColumnCount() - 1);
+						break;
+					}
+				}
+				//else if (event.stateMask == SWT.MOD2) {
+				//selection of multiple rows (Shift+Arrow Up/Down) require DataTableWindow.this.cursor.setVisible(false);
+				//afterwards dataTable.keyReleased handles selection movement
+				//}
+				else if (((event.stateMask & SWT.MOD2) != 0) && ((event.stateMask & SWT.MOD1) != 0)) {
+					switch (event.keyCode) {
+					case SWT.HOME:
+						// Ctrl+Shift+Home: go to first row
+						DataTableWindow.this.cursor.setSelection(0, DataTableWindow.this.cursor.getColumn());
+						break;
+					case SWT.END:
+						// Ctrl+Shift+End: go to last row
+						DataTableWindow.this.cursor.setSelection(DataTableWindow.this.dataTable.getItemCount() - 1, DataTableWindow.this.cursor.getColumn());
+						break;
+					}
+				}
+				else if (event.stateMask == 0) {
+					switch (event.keyCode) {
+					case SWT.HOME:
+						// Home: go to first column
+						workaroundTableCursor(0);
+						break;
+					case SWT.END:
+						// End: go to last column
+						workaroundTableCursor(DataTableWindow.this.dataTable.getColumnCount() - 1);
+						break;
+					}
+				}
+
+				// setSelection() doesn't fire a widgetSelected() event, so we need manually update the vector
+				if (DataTableWindow.this.cursor.getRow() != null) {
+					updateVector(DataTableWindow.this.dataTable.indexOf(DataTableWindow.this.cursor.getRow()), DataTableWindow.this.dataTable.getTopIndex());
+
+					//select the table row, after repositioning cursor (HOME/END)
+					DataTableWindow.this.dataTable.setSelection(new TableItem[] { DataTableWindow.this.cursor.getRow() });
+				}
+			}
+
+			/**
+			 * Problem: When our code for the TableCursor keyReleased() is reached,
+			 * the TableCursor has already internally processed the home/end keyevent
+			 * and changed the row/column, see:
+			 * http://www.javadocexamples.com/org/eclipse/swt/custom/org.eclipse.swt.custom.TableCursor-source.html
+			 * 
+			 * Therefore, we need a Vector that stores the last cell positions, so we can
+			 * access the position before the current position.
+			 * 
+			 * @param col
+			 */
+			private void workaroundTableCursor(int col) {
+				/* Get the second element from the vector.
+				 * This represents the row that was active before the keyevent
+				 * fired and the TableCursor automatically changed the row. 
+				*/
+				int row = DataTableWindow.this.rowVector.get(0);
+				if (log.isLoggable(Level.FINER)) log.log(Level.FINER, ("Setting selection to row: " + row));
+				DataTableWindow.this.cursor.setSelection(row, col);
+
+				/* As the TableCursor automatically changes the rows and we go back, the item that was on top of the list changes.
+				 * We fix that here to get the original item at the top of the list again.
+				 */
+				if (log.isLoggable(Level.FINER))
+					log.log(Level.FINER, ("Setting top index: " + DataTableWindow.this.topindexVector.get(0)));
+				DataTableWindow.this.dataTable.setTopIndex(DataTableWindow.this.topindexVector.get(0));
+
+				/* Workaround: When Home or End is pressed, the cursor is misplaced in such way,
+				 * that the active cell seems to be another one. However, when you press cursor down,
+				 * the cell cursor magically appears one cell below the former expected cell.
+				 * Calling setVisible fixes this problem.
+				 */
+				DataTableWindow.this.cursor.setVisible(true);
+			}
+
+			public void keyPressed(KeyEvent event) {
+				if (log.isLoggable(Level.FINEST)) log.log(Level.FINEST, "cursor.keyPressed " + event); //$NON-NLS-1$
+				//select the table row where the cursor get moved to
+				DataTableWindow.this.dataTable.setSelection(new TableItem[] { DataTableWindow.this.cursor.getRow() });
+
+				//to enable multi selection the cursor needs to be set invisible, if the cursor is invisible the dataTable key listener takes effect
+				//the cursor should be set to invisible only when shift key is pressed, not while shift is pressed in combination with others
+				if (event.keyCode == SWT.MOD2 && ((event.stateMask & SWT.MOD2) == 0) && ((event.stateMask & SWT.MOD1) == 0) && ((event.stateMask & SWT.MOD3) == 0)) {
+					DataTableWindow.this.cursor.setVisible(false);
+					//reset previous (multiple) selection table row(s)
+					DataTableWindow.this.dataTable.setSelection(new TableItem[] { DataTableWindow.this.cursor.getRow() });
+				}
+			}
+		});
+
+		// Add the event handling
+		this.cursor.addSelectionListener(new SelectionAdapter() {
+			// This is called as the user navigates around the table
+			@Override
+			public void widgetSelected(SelectionEvent event) {
+				if (log.isLoggable(Level.FINEST)) log.log(Level.FINEST, "cursor.widgetSelected " + event); //$NON-NLS-1$
+				updateVector(DataTableWindow.this.dataTable.indexOf(DataTableWindow.this.cursor.getRow()), DataTableWindow.this.dataTable.getTopIndex());
+			}
+		});
+
 		this.dataTable.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
 		this.dataTable.addHelpListener(new HelpListener() {
 			public void helpRequested(HelpEvent evt) {
-				if (log.isLoggable(Level.FINER)) log.log(Level.FINER, "DigitalDisplay.helpRequested " + evt); //$NON-NLS-1$
+				if (log.isLoggable(Level.FINER)) log.log(Level.FINER, "dataTable.helpRequested " + evt); //$NON-NLS-1$
 				DataExplorer.getInstance().openHelpDialog("", "HelpInfo_6.html"); //$NON-NLS-1$ //$NON-NLS-2$
 			}
 		});
 		this.dataTable.addListener(SWT.SetData, new Listener() {
 			public void handleEvent(Event event) {
-				Channel activeChannel = channels.getActiveChannel();
+				Channel activeChannel = DataTableWindow.this.channels.getActiveChannel();
 				if (activeChannel != null) {
 					RecordSet activeRecordSet = activeChannel.getActiveRecordSet();
 					if (activeRecordSet != null && activeRecordSet.getRecordDataSize(true) > 0) {
@@ -106,8 +231,78 @@ public class DataTableWindow extends CTabItem {
 				}
 			}
 		});
+		this.dataTable.addKeyListener(new KeyListener() {
+			int	selectionFlowIndex	= 0;
+
+			private void workaroundTableCursor(int col) {
+				/* Get the second element from the vector.
+				 * This represents the row that was active before the keyevent
+				 * fired and the TableCursor automatically changed the row. 
+				*/
+				int row = DataTableWindow.this.rowVector.get(DataTableWindow.this.rowVector.size() - 1);
+				if (log.isLoggable(Level.FINER)) log.log(Level.FINER, ("Setting selection to row: " + row));
+				DataTableWindow.this.cursor.setSelection(row, col);
+
+				/* As the TableCursor automatically changes the rows and we go back, the item that was on top of the list changes.
+				 * We fix that here to get the original item at the top of the list again.
+				 */
+				if (log.isLoggable(Level.FINER))
+					log.log(Level.FINER, ("Setting top index: " + DataTableWindow.this.topindexVector.get(0)));
+				DataTableWindow.this.dataTable.setTopIndex(DataTableWindow.this.topindexVector.get(DataTableWindow.this.topindexVector.size() - 1));
+
+				DataTableWindow.this.cursor.setVisible(true);
+
+				//calling setFocus(releases table key listener and take over to cursor key listener again
+				DataTableWindow.this.cursor.setFocus();
+			}
+
+			public void keyReleased(KeyEvent event) {
+				if (log.isLoggable(Level.FINEST)) log.log(Level.FINEST, ("dataTable.keyReleased, keycode: " + event.keyCode));
+				if (event.keyCode == SWT.MOD2) {
+					int rowIndex = DataTableWindow.this.rowVector.get(DataTableWindow.this.rowVector.size() - 1) + this.selectionFlowIndex;
+					//check table bounds reached
+					rowIndex = rowIndex < 0 ? 0 : rowIndex > DataTableWindow.this.dataTable.getItems().length - 1 ? DataTableWindow.this.dataTable.getItems().length - 1 : rowIndex;
+
+					updateVector(rowIndex, DataTableWindow.this.dataTable.getTopIndex());
+					workaroundTableCursor(DataTableWindow.this.cursor.getColumn());
+					this.selectionFlowIndex = 0;
+				}
+			}
+
+			public void keyPressed(KeyEvent event) {
+				if (log.isLoggable(Level.FINEST)) log.log(Level.FINEST, ("dataTable.keyPressed, keycode: " + event.keyCode));
+				if (event.stateMask == SWT.MOD2 && ((event.stateMask & SWT.MOD1) == 0) && ((event.stateMask & SWT.MOD3) == 0)) {
+					switch (event.keyCode) {
+					case SWT.ARROW_UP:
+						--this.selectionFlowIndex;
+						break;
+					case SWT.ARROW_DOWN:
+						++this.selectionFlowIndex;
+						break;
+					}
+				}
+			}
+		});
 		this.contextMenu.createMenu(this.popupmenu, TabAreaContextMenu.TYPE_TABLE);
-		this.dataTable.setMenu(this.popupmenu);
+		this.cursor.setMenu(this.popupmenu);
+	}
+
+	/**
+	 * Helper function for workaroundTableCursor()
+	 * @param row
+	 * @param topindex
+	 */
+	private void updateVector(int row, int topindex) {
+
+		if (this.rowVector.size() == 2) {
+			// we only need the last and the current value, so remove the old entries
+			this.rowVector.remove(0);
+			this.topindexVector.remove(0);
+		}
+
+		// now add the new row
+		this.rowVector.addElement(row);
+		this.topindexVector.addElement(topindex);
 	}
 
 	/**
@@ -122,7 +317,7 @@ public class DataTableWindow extends CTabItem {
 		}
 
 		int extentFactor = 9;
-		String time = isAbsoluteDateTime ? Messages.getString(MessageIds.GDE_MSGT0436) : Messages.getString(MessageIds.GDE_MSGT0234); 
+		String time = this.isAbsoluteDateTime ? Messages.getString(MessageIds.GDE_MSGT0436) : Messages.getString(MessageIds.GDE_MSGT0234);
 		this.timeColumn = new TableColumn(this.dataTable, SWT.CENTER);
 		this.timeColumn.setWidth(time.length() * 7);
 		this.timeColumn.setText(time);
@@ -139,10 +334,6 @@ public class DataTableWindow extends CTabItem {
 					TableColumn column = new TableColumn(this.dataTable, SWT.CENTER);
 					column.setWidth(sb.length() * extentFactor);
 					column.setText(sb.toString());
-				}
-				if (System.getProperty("os.name", "").toLowerCase().startsWith("linux")) { // add aditional header field for padding //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-					TableColumn column = new TableColumn(this.dataTable, SWT.CENTER);
-					column.setWidth(100);
 				}
 			}
 			else {
@@ -168,17 +359,16 @@ public class DataTableWindow extends CTabItem {
 	 * @param count of record entries
 	 */
 	public void setRowCount(int count) {
-		this.dataTable.setItemCount (count);
+		this.dataTable.setItemCount(count);
 	}
-	
+
 	/**
 	 * clean the table, not the header
 	 */
 	public synchronized void cleanTable() {
-		if (this.dataTable != null && !this.dataTable.isDisposed())
-			this.dataTable.removeAll();
+		if (this.dataTable != null && !this.dataTable.isDisposed()) this.dataTable.removeAll();
 	}
-	
+
 	/**
 	 * create visible tab window content as image
 	 * @return image with content
@@ -192,7 +382,7 @@ public class DataTableWindow extends CTabItem {
 
 		return tabContentImage;
 	}
-	
+
 	/**
 	 * reset the top index of the data table if required while updating or adding rows
 	 */
@@ -203,8 +393,7 @@ public class DataTableWindow extends CTabItem {
 		int itemCount = this.dataTable.getItemCount();
 		if (itemCount > visibleItemCount) {
 			int newTopIndex = itemCount - visibleItemCount;
-			if (topIndex != newTopIndex)
-				this.dataTable.setTopIndex(newTopIndex);
+			if (topIndex != newTopIndex) this.dataTable.setTopIndex(newTopIndex);
 		}
 	}
 
@@ -214,4 +403,5 @@ public class DataTableWindow extends CTabItem {
 	public void setAbsoluteDateTime(boolean isAbsoluteDateTime) {
 		this.isAbsoluteDateTime = isAbsoluteDateTime;
 	}
+
 }
