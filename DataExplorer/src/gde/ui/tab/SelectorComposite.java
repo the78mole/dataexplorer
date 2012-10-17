01cc2515adc35d16d7149af73f09efa11ced84dd
Winfried Bruegmann <winfried.bruegmann@web.de>
2012-10-17 08:38:02 +0000

    switch off all curves at once

--- DataExplorer/src/gde/ui/tab/SelectorComposite.java
+++ DataExplorer/src/gde/ui/tab/SelectorComposite.java
@@ -118,7 +118,7 @@ public class SelectorComposite extends Composite {
 			FormData curveSelectorHeaderLData = new FormData();
 			curveSelectorHeaderLData.width = this.initialSelectorHeaderWidth;
 			curveSelectorHeaderLData.height = 25;
-			curveSelectorHeaderLData.left = new FormAttachment(0, 1000, GDE.IS_WINDOWS ? 5 : 0);
+			curveSelectorHeaderLData.left = new FormAttachment(0, 1000, GDE.IS_WINDOWS ? 6 : 0);
 			curveSelectorHeaderLData.top = new FormAttachment(0, 1000, 0);
 			this.curveSelectorHeader.setLayoutData(curveSelectorHeaderLData);
 			this.curveSelectorHeader.setBackground(DataExplorer.COLOR_LIGHT_GREY);
