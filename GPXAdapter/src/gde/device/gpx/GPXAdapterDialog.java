7650524fe5aacba5f5ea1766de368f11644cfefc
Winfried Bruegmann <winfried.bruegmann@web.de>
2013-01-09 12:53:55 +0000

    add GPXAdapter

---
+++ GPXAdapter/src/gde/device/gpx/GPXAdapterDialog.java
@@ -0,0 +1,316 @@
+/**************************************************************************************
+  	This file is part of GNU DataExplorer.
+
+    GNU DataExplorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GNU DataExplorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Copyright (c) 2010,2011,2012 Winfried Bruegmann
+****************************************************************************************/
+package gde.device.gpx;
+
+import gde.GDE;
+import gde.config.Settings;
+import gde.data.Channels;
+import gde.data.RecordSet;
+import gde.device.DeviceDialog;
+import gde.device.InputTypes;
+import gde.log.Level;
+import gde.messages.Messages;
+import gde.ui.SWTResourceManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.HelpEvent;
+import org.eclipse.swt.events.HelpListener;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Dialog class to enable visualization control
+ * @author Winfried Brügmann
+ */
+public class GPXAdapterDialog extends DeviceDialog {
+	final static Logger						log									= Logger.getLogger(GPXAdapterDialog.class.getName());
+
+	CTabFolder										tabFolder, subTabFolder1, subTabFolder2;
+	CTabItem											visualizationTabItem;
+	Composite											visualizationMainComposite, uniLogVisualization, mLinkVisualization;
+	Composite											configurationMainComposite;
+
+	Button												saveVisualizationButton, inputFileButton, helpButton, closeButton;
+
+	CTabItem											gpsLoggerTabItem, telemetryTabItem;
+
+	final GPXAdapter								device;																																						// get device specific things, get serial port, ...
+	final Settings								settings;																																					// application configuration settings
+
+	RecordSet											lastActiveRecordSet		= null;
+	boolean												isVisibilityChanged	= false;
+	int														measurementsCount		= 0;
+	final List<CTabItem>					configurations			= new ArrayList<CTabItem>();
+
+	/**
+	 * default constructor initialize all variables required
+	 * @param parent Shell
+	 * @param useDevice device specific class implementation
+	 */
+	public GPXAdapterDialog(Shell parent, GPXAdapter useDevice) {
+		super(parent);
+		this.device = useDevice;
+		this.settings = Settings.getInstance();
+		this.measurementsCount = (Math.abs(this.device.getDataBlockSize(InputTypes.FILE_IO))+1)/2;
+	}
+
+	@Override
+	public void open() {
+		try {
+			this.shellAlpha = Settings.getInstance().getDialogAlphaValue();
+			this.isAlphaEnabled = Settings.getInstance().isDeviceDialogAlphaEnabled();
+
+			log.log(java.util.logging.Level.FINE, "dialogShell.isDisposed() " + ((this.dialogShell == null) ? "null" : this.dialogShell.isDisposed())); //$NON-NLS-1$ //$NON-NLS-2$
+			if (this.dialogShell == null || this.dialogShell.isDisposed()) {
+				if (this.settings.isDeviceDialogsModal())
+					this.dialogShell = new Shell(this.application.getShell(), SWT.DIALOG_TRIM | SWT.PRIMARY_MODAL);
+				else if (this.settings.isDeviceDialogsOnTop())
+					this.dialogShell = new Shell(this.application.getDisplay(), SWT.DIALOG_TRIM | SWT.ON_TOP);
+				else
+					this.dialogShell = new Shell(this.application.getDisplay(), SWT.DIALOG_TRIM);
+
+				SWTResourceManager.registerResourceUser(this.dialogShell);
+
+				FormLayout dialogShellLayout = new FormLayout();
+				this.dialogShell.setLayout(dialogShellLayout);
+				this.dialogShell.layout();
+				this.dialogShell.pack();
+				this.dialogShell.setSize(GDE.IS_LINUX ? 740 : 675, 30 + 25 + 25 + this.measurementsCount * 26 + 50 + 42); //header + tab + label + this.measurementsCount * 26 + buttons
+				this.dialogShell.setText(this.device.getName() + Messages.getString(gde.messages.MessageIds.GDE_MSGT0273));
+				this.dialogShell.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
+				this.dialogShell.setImage(SWTResourceManager.getImage("gde/resource/ToolBoxHot.gif")); //$NON-NLS-1$
+				this.dialogShell.addListener(SWT.Traverse, new Listener() {
+		      public void handleEvent(Event event) {
+		        switch (event.detail) {
+		        case SWT.TRAVERSE_ESCAPE:
+		        	GPXAdapterDialog.this.dialogShell.close();
+		          event.detail = SWT.TRAVERSE_NONE;
+		          event.doit = false;
+		          break;
+		        }
+		      }
+		    });
+				this.dialogShell.addDisposeListener(new DisposeListener() {
+					public void widgetDisposed(DisposeEvent evt) {
+						log.log(java.util.logging.Level.FINEST, "dialogShell.widgetDisposed, event=" + evt); //$NON-NLS-1$
+						if (GPXAdapterDialog.this.device.isChangePropery()) {
+							String msg = Messages.getString(gde.messages.MessageIds.GDE_MSGI0041, new String[] { GPXAdapterDialog.this.device.getPropertiesFileName() });
+							if (GPXAdapterDialog.this.application.openYesNoMessageDialog(getDialogShell(), msg) == SWT.YES) {
+								log.log(java.util.logging.Level.FINE, "SWT.YES"); //$NON-NLS-1$
+								GPXAdapterDialog.this.device.storeDeviceProperties();
+								setClosePossible(true);
+							}
+						}
+						GPXAdapterDialog.this.dispose();
+					}
+				});
+				this.dialogShell.addHelpListener(new HelpListener() {
+					public void helpRequested(HelpEvent evt) {
+						log.log(java.util.logging.Level.FINER, "dialogShell.helpRequested, event=" + evt); //$NON-NLS-1$
+						GPXAdapterDialog.this.application.openHelpDialog(GPXAdapterDialog.this.device.getName(), "HelpInfo.html");  //$NON-NLS-1$
+					}
+				});
+				this.dialogShell.addPaintListener(new PaintListener() {
+					public void paintControl(PaintEvent paintevent) {
+						if (log.isLoggable(Level.FINEST)) log.log(Level.FINEST, "dialogShell.paintControl, event=" + paintevent); //$NON-NLS-1$
+						RecordSet activeRecordSet = GPXAdapterDialog.this.application.getActiveRecordSet();
+						if (GPXAdapterDialog.this.lastActiveRecordSet == null && activeRecordSet != null 
+								|| ( activeRecordSet != null && !GPXAdapterDialog.this.lastActiveRecordSet.getName().equals(activeRecordSet.getName()))) {
+							GPXAdapterDialog.this.tabFolder.setSelection(Channels.getInstance().getActiveChannelNumber() - 1);
+						}
+						GPXAdapterDialog.this.lastActiveRecordSet = GPXAdapterDialog.this.application.getActiveRecordSet();
+					}
+				});
+				{
+					this.tabFolder = new CTabFolder(this.dialogShell, SWT.NONE);
+					this.tabFolder.setSimple(false);
+					{
+						for (int i = 1; i <= this.device.getChannelCount(); i++) {
+							createVisualizationTabItem(i, 24);
+						}
+					}
+					FormData tabFolderLData = new FormData();
+					tabFolderLData.top = new FormAttachment(0, 1000, 0);
+					tabFolderLData.left = new FormAttachment(0, 1000, 0);
+					tabFolderLData.right = new FormAttachment(1000, 1000, 0);
+					tabFolderLData.bottom = new FormAttachment(1000, 1000, -50);
+					this.tabFolder.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
+					this.tabFolder.setLayoutData(tabFolderLData);
+					this.tabFolder.setSelection(0);
+				}
+				{
+					this.saveVisualizationButton = new Button(this.dialogShell, SWT.PUSH | SWT.CENTER);
+					FormData saveButtonLData = new FormData();
+					saveButtonLData.width = 130;
+					saveButtonLData.height = GDE.IS_MAC ? 33 : 30;
+					saveButtonLData.left = new FormAttachment(0, 1000, 15);
+					saveButtonLData.bottom = new FormAttachment(1000, 1000, GDE.IS_MAC ? -8 : -10);
+					this.saveVisualizationButton.setLayoutData(saveButtonLData);
+					this.saveVisualizationButton.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
+					this.saveVisualizationButton.setText(Messages.getString(MessageIds.GDE_MSGT1786));
+					this.saveVisualizationButton.setToolTipText(Messages.getString(MessageIds.GDE_MSGT1787));
+					this.saveVisualizationButton.setEnabled(false);
+					this.saveVisualizationButton.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(SelectionEvent evt) {
+							log.log(java.util.logging.Level.FINEST, "saveButton.widgetSelected, event=" + evt); //$NON-NLS-1$
+							GPXAdapterDialog.this.device.storeDeviceProperties();
+							GPXAdapterDialog.this.saveVisualizationButton.setEnabled(false);
+						}
+					});
+				}
+				{
+					this.inputFileButton = new Button(this.dialogShell, SWT.PUSH | SWT.CENTER);
+					FormData inputFileButtonLData = new FormData();
+					inputFileButtonLData.width = 130;
+					inputFileButtonLData.height = GDE.IS_MAC ? 33 : 30;
+					inputFileButtonLData.left = new FormAttachment(0, 1000, 155);
+					inputFileButtonLData.bottom = new FormAttachment(1000, 1000, GDE.IS_MAC ? -8 : -10);
+					this.inputFileButton.setLayoutData(inputFileButtonLData);
+					this.inputFileButton.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
+					this.inputFileButton.setText(Messages.getString(MessageIds.GDE_MSGT1788));
+					this.inputFileButton.setToolTipText(Messages.getString(MessageIds.GDE_MSGT1789));
+					this.inputFileButton.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(SelectionEvent evt) {
+							log.log(java.util.logging.Level.FINEST, "inputFileButton.widgetSelected, event=" + evt); //$NON-NLS-1$
+							if (GPXAdapterDialog.this.isVisibilityChanged) {
+								String msg = Messages.getString(gde.messages.MessageIds.GDE_MSGI0041, new String[] { GPXAdapterDialog.this.device.getPropertiesFileName() });
+								if (GPXAdapterDialog.this.application.openYesNoMessageDialog(GPXAdapterDialog.this.dialogShell, msg) == SWT.YES) {
+									log.log(java.util.logging.Level.FINE, "SWT.YES"); //$NON-NLS-1$
+									GPXAdapterDialog.this.device.storeDeviceProperties();
+								}
+							}
+							GPXAdapterDialog.this.device.open_closeCommPort();
+						}
+					});
+				}
+				{
+					this.helpButton = new Button(this.dialogShell, SWT.PUSH | SWT.CENTER);
+					FormData helpButtonLData = new FormData();
+					helpButtonLData.width = GDE.IS_MAC ? 50 : 40;
+					helpButtonLData.height = GDE.IS_MAC ? 33 : 30;
+					helpButtonLData.left = new FormAttachment(0, 1000, 302);
+					helpButtonLData.bottom = new FormAttachment(1000, 1000, GDE.IS_MAC ? -8 : -10);
+					this.helpButton.setLayoutData(helpButtonLData);
+					this.helpButton.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
+					this.helpButton.setImage(SWTResourceManager.getImage("gde/resource/QuestionHot.gif")); //$NON-NLS-1$
+					this.helpButton.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(SelectionEvent evt) {
+							log.log(java.util.logging.Level.FINEST, "helpButton.widgetSelected, event=" + evt); //$NON-NLS-1$
+							GPXAdapterDialog.this.application.openHelpDialog(GPXAdapterDialog.this.device.getName(), "HelpInfo.html");  //$NON-NLS-1$
+						}
+					});
+				}
+				{
+					this.closeButton = new Button(this.dialogShell, SWT.PUSH | SWT.CENTER);
+					FormData closeButtonLData = new FormData();
+					closeButtonLData.width = 130;
+					closeButtonLData.height = GDE.IS_MAC ? 33 : 30;
+					closeButtonLData.right = new FormAttachment(1000, 1000, -10);
+					closeButtonLData.bottom = new FormAttachment(1000, 1000, GDE.IS_MAC ? -8 : -10);
+					this.closeButton.setLayoutData(closeButtonLData);
+					this.closeButton.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
+					this.closeButton.setText(Messages.getString(gde.messages.MessageIds.GDE_MSGT0485));
+					this.closeButton.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(SelectionEvent evt) {
+							log.log(java.util.logging.Level.FINEST, "closeButton.widgetSelected, event=" + evt); //$NON-NLS-1$
+							GPXAdapterDialog.this.dispose();
+						}
+					});
+				}
+
+				this.dialogShell.setLocation(getParent().toDisplay(getParent().getSize().x / 2 - 375, 10));
+				this.dialogShell.open();
+			}
+			else {
+				this.dialogShell.setVisible(true);
+				this.dialogShell.setActive();
+			}
+			Display display = this.dialogShell.getDisplay();
+			while (!this.dialogShell.isDisposed()) {
+				if (!display.readAndDispatch()) display.sleep();
+			}
+		}
+		catch (Exception e) {
+			log.log(java.util.logging.Level.SEVERE, e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * create a visualization control tab item
+	 * @param channelNumber
+	 */
+	private void createVisualizationTabItem(int channelNumber, int numMeasurements) {
+		this.visualizationTabItem = new CTabItem(this.tabFolder, SWT.NONE);
+		this.visualizationTabItem.setFont(SWTResourceManager.getFont(GDE.WIDGET_FONT_NAME, GDE.WIDGET_FONT_SIZE, SWT.NORMAL));
+		this.visualizationTabItem.setText(Messages.getString(MessageIds.GDE_MSGT1785) + GDE.STRING_MESSAGE_CONCAT + this.device.getChannelName(channelNumber));
+
+		this.visualizationMainComposite = new Composite(this.tabFolder, SWT.NONE);
+		FormLayout visualizationMainCompositeLayout = new FormLayout();
+		this.visualizationMainComposite.setLayout(visualizationMainCompositeLayout);
+		this.visualizationTabItem.setControl(this.visualizationMainComposite);
+		{
+			FormData layoutData = new FormData();
+			layoutData.top = new FormAttachment(0, 1000, 0);
+			layoutData.left = new FormAttachment(0, 1000, 0);
+			layoutData.right = new FormAttachment(1000, 1000, 0);
+			layoutData.bottom = new FormAttachment(1000, 1000, 0);
+			new VisualizationControl(this.visualizationMainComposite, layoutData, this, channelNumber, this.device, Messages.getString(MessageIds.GDE_MSGT1785), 0, numMeasurements);
+
+		}
+	}
+
+	/**
+	 * set the save visualization configuration button enabled 
+	 */
+	@Override
+	public void enableSaveButton(boolean enable) {
+		this.saveVisualizationButton.setEnabled(enable);
+		this.application.updateAllTabs(true);
+	}
+
+	/**
+	 * @return the tabFolder selection index
+	 */
+	public Integer getTabFolderSelectionIndex() {
+		return this.tabFolder.getSelectionIndex();
+	}
+}
---
+++ GPXAdapter/src/gde/device/gpx/MessageIds.java
@@ -0,0 +1,40 @@
+/************************************************************************************** 
+  	This file is part of GNU DataExplorer.
+
+		GNU DataExplorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DataExplorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) 2008,2009,2010,2011,2012 Winfried Bruegmann 
+****************************************************************************************/
+package gde.device.gpx;
+
+/** 
+ * @author Winfried Brügmann 
+ * Do not edit, MessageIds are generated from messages.properties 
+ */ 
+public class MessageIds { 
+
+	public final static String	GDE_MSGT1776 = "GDE_MSGT1776";
+	public final static String	GDE_MSGT1779 = "GDE_MSGT1779";
+	public final static String	GDE_MSGT1780 = "GDE_MSGT1780";
+	public final static String	GDE_MSGT1781 = "GDE_MSGT1781";
+	public final static String	GDE_MSGT1782 = "GDE_MSGT1782";
+	public final static String	GDE_MSGT1783 = "GDE_MSGT1783";
+	public final static String	GDE_MSGT1784 = "GDE_MSGT1784";
+	public final static String	GDE_MSGT1785 = "GDE_MSGT1785";
+	public final static String	GDE_MSGT1786 = "GDE_MSGT1786";
+	public final static String	GDE_MSGT1787 = "GDE_MSGT1787";
+	public final static String	GDE_MSGT1788 = "GDE_MSGT1788";
+	public final static String	GDE_MSGT1789 = "GDE_MSGT1789";
+
+ }
