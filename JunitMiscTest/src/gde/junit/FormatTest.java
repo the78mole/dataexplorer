3bedf392a88c10cd3b004fa7578e22bbd9efdcb4
Winfried Bruegmann <winfried.bruegmann@web.de>
2013-01-15 20:58:56 +0000

    misc updates

--- JunitMiscTest/src/gde/junit/FormatTest.java
+++ JunitMiscTest/src/gde/junit/FormatTest.java
@@ -15,7 +15,7 @@ package gde.junit;
     You should have received a copy of the GNU General Public License
     along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
     
-    Copyright (c) 2008,2009,2010,2011,2012 Winfried Bruegmann
+    Copyright (c) 2008,2009,2010,2011,2012,2013 Winfried Bruegmann
 ****************************************************************************************/
 import java.util.Locale;
 
