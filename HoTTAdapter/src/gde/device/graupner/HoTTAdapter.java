26a9aacaad13136776a5d194098eff6395a64744
Winfried Bruegmann <winfried.bruegmann@web.de>
2013-05-13 16:23:36 +0000

    enable MDL Viewer tabulator

--- HoTTAdapter/src/gde/device/graupner/HoTTAdapter.java
+++ HoTTAdapter/src/gde/device/graupner/HoTTAdapter.java
@@ -1258,7 +1258,10 @@ public class HoTTAdapter extends DeviceConfiguration implements IDevice {
 			log.log(Level.OFF, "loading TabItem " + ((CTabItem)inst).getText()); //$NON-NLS-1$
 		
 		//set directory where to start search for mdl files
+		System.setProperty("logs.dir", Settings.getInstance().getLogFilePath().substring(0, Settings.getInstance().getLogFilePath().lastIndexOf(GDE.FILE_SEPARATOR_UNIX)));
+		log.log(Level.OFF, "logs.dir =  " + System.getProperty("logs.dir")); //$NON-NLS-1$
 		System.setProperty("program.dir", Settings.getInstance().getDataFilePath());
+		log.log(Level.OFF, "program.dir =  " + System.getProperty("program.dir")); //$NON-NLS-1$
 
 		return (CTabItem)inst;
 	}
