c5a7a16fce4cc48fb2b97fee2b61340d4521db18
Winfried Bruegmann <winfried.bruegmann@web.de>
2013-07-16 10:33:31 +0000

    add 4D-Akkumonitor

--- CSV2SerialAdapter/src/gde/device/wb/AkkuMonitor.java
+++ CSV2SerialAdapter/src/gde/device/wb/AkkuMonitor.java
@@ -1,16 +1,41 @@
+/**************************************************************************************
+  	This file is part of GNU DataExplorer.
+
+    GNU DataExplorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GNU DataExplorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Copyright (c) 2012,2013 Winfried Bruegmann
+****************************************************************************************/
 package gde.device.wb;
 
+import gde.GDE;
 import gde.data.Record;
 import gde.data.RecordSet;
 import gde.device.DeviceConfiguration;
+import gde.device.InputTypes;
 import gde.device.MeasurementPropertyTypes;
 import gde.device.PropertyType;
+import gde.io.CSVSerialDataReaderWriter;
 import gde.log.Level;
+import gde.messages.Messages;
+import gde.utils.FileUtils;
 import gde.utils.StringHelper;
 
 import java.io.FileNotFoundException;
 
 import javax.xml.bind.JAXBException;
+
+import org.eclipse.swt.widgets.FileDialog;
 
 public class AkkuMonitor extends CSV2SerialAdapter {
 
@@ -174,5 +199,57 @@ public class AkkuMonitor extends CSV2SerialAdapter {
 
 		log.log(Level.FINER, "for " + record.getName() + " in value = " + value + " out value = " + newValue); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		return newValue;
+	}
+
+	/**
+	 * import a CSV file, also called "OpenFormat" file
+	 */
+	@Override
+	public void importCsvFiles() {
+		final FileDialog fd = FileUtils.getImportDirectoryFileDialog(this, Messages.getString(MessageIds.GDE_MSGT1700));
+
+		Thread reader = new Thread("reader") { //$NON-NLS-1$
+			@Override
+			public void run() {
+				try {
+					AkkuMonitor.this.application.setPortConnected(true);
+					for (String tmpFileName : fd.getFileNames()) {
+						String selectedImportFile = fd.getFilterPath() + GDE.FILE_SEPARATOR_UNIX + tmpFileName;
+						log.log(Level.FINE, "selectedImportFile = " + selectedImportFile); //$NON-NLS-1$
+
+						if (fd.getFileName().length() > 4) {
+							try {
+								String recordNameExtend;
+								try {
+									recordNameExtend = selectedImportFile.substring(selectedImportFile.lastIndexOf(GDE.STRING_DOT)-4, selectedImportFile.lastIndexOf(GDE.STRING_DOT));
+									Integer.valueOf(recordNameExtend);
+								}
+								catch (Exception e) {
+									try {
+										recordNameExtend = selectedImportFile.substring(selectedImportFile.lastIndexOf(GDE.STRING_DOT)-3, selectedImportFile.lastIndexOf(GDE.STRING_DOT));
+										Integer.valueOf(recordNameExtend);
+									}
+									catch (Exception e1) {
+										recordNameExtend = GDE.STRING_EMPTY;
+									}
+								}
+								Integer channelConfigNumber = dialog != null && !dialog.isDisposed() ? dialog.getTabFolderSelectionIndex() + 1 : null;
+								CSVSerialDataReaderWriter.read(selectedImportFile, AkkuMonitor.this, recordNameExtend, channelConfigNumber, 
+										new AkkuMonitorParser(AkkuMonitor.this.getDataBlockTimeUnitFactor(), 
+												AkkuMonitor.this.getDataBlockLeader(), AkkuMonitor.this.getDataBlockSeparator().value(), 
+												AkkuMonitor.this.getDataBlockCheckSumType(), AkkuMonitor.this.getDataBlockSize(InputTypes.FILE_IO)));
+							}
+							catch (Throwable e) {
+								log.log(Level.WARNING, e.getMessage(), e);
+							}
+						}
+					}
+				}
+				finally {
+					AkkuMonitor.this.application.setPortConnected(false);
+				}
+			}
+		};
+		reader.start();
 	}
 }
---
+++ CSV2SerialAdapter/src/gde/device/wb/AkkuMonitorParser.java
@@ -0,0 +1,121 @@
+/**************************************************************************************
+  	This file is part of GNU DataExplorer.
+
+    GNU DataExplorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GNU DataExplorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Copyright (c) 2012,2013 Winfried Bruegmann
+****************************************************************************************/
+package gde.device.wb;
+
+import gde.GDE;
+import gde.device.CheckSumTypes;
+import gde.device.FormatTypes;
+import gde.exception.DevicePropertiesInconsistenceException;
+import gde.io.DataParser;
+import gde.messages.MessageIds;
+import gde.messages.Messages;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * implements special requirements for AkkuMonitor data
+ */
+public class AkkuMonitorParser extends DataParser {
+	private static final Logger	log												= Logger.getLogger(AkkuMonitorParser.class.getName());
+
+	/**
+	 * @param useTimeFactor
+	 * @param useLeaderChar
+	 * @param useSeparator
+	 * @param useCheckSumType
+	 * @param useDataSize
+	 */
+	public AkkuMonitorParser(int useTimeFactor, String useLeaderChar, String useSeparator, CheckSumTypes useCheckSumType, int useDataSize) {
+		super(useTimeFactor, useLeaderChar, useSeparator, useCheckSumType, useDataSize);
+	}
+
+	/**
+	 * @param useTimeFactor
+	 * @param useLeaderChar
+	 * @param useSeparator
+	 * @param useCheckSumType
+	 * @param useCheckSumFormatType
+	 * @param useDataSize
+	 * @param useDataFormatType
+	 * @param doMultiply1000
+	 */
+	public AkkuMonitorParser(int useTimeFactor, String useLeaderChar, String useSeparator, CheckSumTypes useCheckSumType, FormatTypes useCheckSumFormatType, int useDataSize,
+			FormatTypes useDataFormatType, boolean doMultiply1000) {
+		super(useTimeFactor, useLeaderChar, useSeparator, useCheckSumType, useCheckSumFormatType, useDataSize, useDataFormatType, doMultiply1000);
+	}
+
+	/**
+	 * default parse method for $1, 1, 0, 14780, 0,598, 1,000, 8,838, 22 like lines
+	 * @param inputLine
+	 * @param strValues
+	 * @throws DevicePropertiesInconsistenceException
+	 */
+	@Override
+	public void parse(String inputLine, String[] strValues) throws DevicePropertiesInconsistenceException {
+		String strValue = strValues[0].trim().substring(1);
+		this.channelConfigNumber = Integer.parseInt(strValue);
+
+		strValue = strValues[1].trim();
+		this.state = Integer.parseInt(strValue);
+
+		strValue = strValues[2].trim().replace(GDE.STRING_COMMA, GDE.STRING_DOT);
+		strValue = strValue.length() > 0 ? strValue : "0";
+		if (this.start_time_ms == Integer.MIN_VALUE) {
+			this.start_time_ms = (int) (Double.parseDouble(strValue) * this.timeFactor); // Seconds * 1000 = msec
+		}
+		else {
+			this.time_ms = (int) (Double.parseDouble(strValue) * this.timeFactor) - this.start_time_ms; // Seconds * 1000 = msec			
+		}
+
+		for (int i = 0; i < this.valueSize; i++) {
+			strValue = strValues[i + 3].trim();
+			try {
+				double tmpValue = strValue.length() > 0 ? Double.parseDouble(strValue.trim()) : 0.0;
+				if (this.isMultiply1000 && tmpValue < Integer.MAX_VALUE / 1000 && tmpValue > Integer.MIN_VALUE / 1000)
+					this.values[i] = (int) (tmpValue * 1000); // enable 3 positions after decimal place
+				else // needs special processing within IDevice.translateValue(), IDevice.reverseTranslateValue()
+				if (tmpValue < Integer.MAX_VALUE || tmpValue > Integer.MIN_VALUE) {
+					this.values[i] = (int) tmpValue;
+				}
+				else {
+					this.values[i] = (int) (tmpValue / 1000);
+				}
+			}
+			catch (NumberFormatException e) {
+				this.values[i] = 0;
+			}
+		}
+
+		//check time reset to force a new data set creation
+		if (this.device.getTimeStep_ms() < 0 && this.time_ms <= 0 && this.isTimeResetEnabled) {
+			this.recordSetNumberOffset += ++this.timeResetCounter;
+			this.isTimeResetEnabled = false;
+		}
+
+		if (this.checkSumType != null) {
+			if (!isChecksumOK(inputLine, Integer.parseInt(strValues[strValues.length - 1].trim(), 16))) {
+				DevicePropertiesInconsistenceException e = new DevicePropertiesInconsistenceException(Messages.getString(MessageIds.GDE_MSGE0049, new Object[] { strValues[strValues.length - 1].trim(), String.format("%X", calcChecksum(inputLine)) }));
+				log.log(Level.WARNING, e.getMessage(), e);
+				throw e;
+			}
+		}
+	}
+
+}
