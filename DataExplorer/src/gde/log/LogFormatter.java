2fe1f9ea09792b12764fbb81f99b0ad1cab047e2
Winfried Bruegmann <winfried.bruegmann@web.de>
2014-01-25 16:28:05 +0000

    update Copyright 2014

--- DataExplorer/src/gde/log/LogFormatter.java
+++ DataExplorer/src/gde/log/LogFormatter.java
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
     
-    Copyright (c) 2008,2009,2010,2011,2012,2013 Winfried Bruegmann
+    Copyright (c) 2008,2009,2010,2011,2012,2013,2014 Winfried Bruegmann
 ****************************************************************************************/
 package gde.log;
 
