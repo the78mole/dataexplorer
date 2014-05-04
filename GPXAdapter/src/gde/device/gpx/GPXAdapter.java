8a9e82a65d96b913354e4108d57fa85f75a14f27
Winfried Bruegmann <winfried.bruegmann@web.de>
2014-05-04 08:05:36 +0000

    final remove export capability for IGC files

--- GPXAdapter/src/gde/device/gpx/GPXAdapter.java
+++ GPXAdapter/src/gde/device/gpx/GPXAdapter.java
@@ -37,7 +37,6 @@ import gde.io.LogViewReader;
 import gde.io.NMEAParser;
 import gde.messages.Messages;
 import gde.ui.DataExplorer;
-import gde.ui.dialog.IgcExportDialog;
 import gde.utils.FileUtils;
 import gde.utils.StringHelper;
 
@@ -597,7 +596,6 @@ public class GPXAdapter extends DeviceConfiguration implements IDevice {
 	public void updateFileMenu(Menu exportMenue) {
 		MenuItem convertKMZ3DRelativeItem;
 		MenuItem convertKMZ3DAbsoluteItem;
-		MenuItem convertIGCItem;
 
 		if (exportMenue.getItem(exportMenue.getItemCount() - 1).getText().equals(Messages.getString(gde.messages.MessageIds.GDE_MSGT0018))) {
 			new MenuItem(exportMenue, SWT.SEPARATOR);
@@ -629,22 +627,6 @@ public class GPXAdapter extends DeviceConfiguration implements IDevice {
 				public void handleEvent(Event e) {
 					GPXAdapter.log.log(java.util.logging.Level.FINEST, "convertKLM3DAbsoluteItem action performed! " + e); //$NON-NLS-1$
 					export2KMZ3D(DeviceConfiguration.HEIGHT_CLAMPTOGROUND);
-				}
-			});
-
-			new MenuItem(exportMenue, SWT.SEPARATOR);
-
-			convertIGCItem = new MenuItem(exportMenue, SWT.PUSH);
-			convertIGCItem.setText(Messages.getString(gde.messages.MessageIds.GDE_MSGT0611));
-			convertIGCItem.addListener(SWT.Selection, new Listener() {
-				@Override
-				public void handleEvent(Event e) {
-					GPXAdapter.log.log(java.util.logging.Level.FINEST, "convertIGCItem action performed! " + e); //$NON-NLS-1$
-					//GPS 		0=latitude 1=longitude 2=altitudeAbs 3=numSatelites 4=PDOP 5=HDOP 6=VDOP 7=velocity;
-					//SMGPS 	8=altitudeRel 9=climb 10=voltageRx 11=distanceTotal 12=distanceStart 13=directionStart 14=glideRatio;
-					//Unilog 15=voltageUniLog 16=currentUniLog 17=powerUniLog 18=revolutionUniLog 19=voltageRxUniLog 20=heightUniLog 21=a1UniLog 22=a2UniLog 23=a3UniLog;
-					//M-LINK 24=valAdd00 25=valAdd01 26=valAdd02 27=valAdd03 28=valAdd04 29=valAdd05 30=valAdd06 31=valAdd07 32=valAdd08 33=valAdd09 34=valAdd10 35=valAdd11 36=valAdd12 37=valAdd13 38=valAdd14;
-					new IgcExportDialog().open(1, 0, 2);
 				}
 			});
 		}
