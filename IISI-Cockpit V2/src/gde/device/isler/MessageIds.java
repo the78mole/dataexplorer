8fe0ad7a8aa3e9115a162363b556afed5d669e9a
Winfried Bruegmann <winfried.bruegmann@web.de>
2014-07-09 18:02:42 +0000

    add IISI Cockpit V2 support

---
+++ IISI-Cockpit V2/src/gde/device/isler/MessageIds.java
@@ -0,0 +1,47 @@
+/************************************************************************************** 
+  	This file is part of GNU DataExplorer.
+
+		GNU DataExplorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DataExplorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) 2008,2009,2010,2011,2012,2013,2014 Winfried Bruegmann 
+****************************************************************************************/
+package gde.device.isler;
+
+/** 
+ * @author Winfried Brügmann 
+ * Do not edit, MessageIds are generated from messages.properties 
+ */ 
+public class MessageIds { 
+
+	public final static String	GDE_MSGW3000 = "GDE_MSGW3000";
+	public final static String	GDE_MSGW3001 = "GDE_MSGW3001";
+	public final static String	GDE_MSGI3000 = "GDE_MSGI3000";
+	public final static String	GDE_MSGI3001 = "GDE_MSGI3001";
+	public final static String	GDE_MSGI3002 = "GDE_MSGI3002";
+	public final static String	GDE_MSGI3003 = "GDE_MSGI3003";
+	public final static String	GDE_MSGT3000 = "GDE_MSGT3000";
+	public final static String	GDE_MSGT3001 = "GDE_MSGT3001";
+	public final static String	GDE_MSGT3002 = "GDE_MSGT3002";
+	public final static String	GDE_MSGT3003 = "GDE_MSGT3003";
+	public final static String	GDE_MSGT3004 = "GDE_MSGT3004";
+	public final static String	GDE_MSGT3005 = "GDE_MSGT3005";
+	public final static String	GDE_MSGT3006 = "GDE_MSGT3006";
+	public final static String	GDE_MSGT3008 = "GDE_MSGT3008";
+	public final static String	GDE_MSGT3009 = "GDE_MSGT3009";
+	public final static String	GDE_MSGT3010 = "GDE_MSGT3010";
+	public final static String	GDE_MSGT3011 = "GDE_MSGT3011";
+	public final static String	GDE_MSGT3012 = "GDE_MSGT3012";
+	public final static String	GDE_MSGT3013 = "GDE_MSGT3013";
+
+ }
