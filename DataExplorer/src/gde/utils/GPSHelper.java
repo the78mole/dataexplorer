b0c08d4348bda567c7e51d34fcf02eb11112c60f
Winfried Bruegmann <winfried.bruegmann@web.de>
2015-10-21 19:29:48 +0000

    test code start line and laps counting

--- DataExplorer/src/gde/utils/GPSHelper.java
+++ DataExplorer/src/gde/utils/GPSHelper.java
@@ -24,6 +24,9 @@ import gde.device.IDevice;
 import gde.log.Level;
 
 import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.Vector;
 import java.util.logging.Logger;
 
@@ -34,6 +37,32 @@ public class GPSHelper {
 	private static Logger log = Logger.getLogger(GPSHelper.class.getName());
 	final static double rad = Math.PI / 180;
 	
+	public static class LatLong {
+		//GPS coordinate needs to be in degrees
+		double _latitude;
+		double _longitude;
+		
+		public LatLong(final double latitude, final double Longitude) {
+			this._latitude = latitude;
+			this._longitude = Longitude;
+		}
+		public double getLatitude() {
+			return _latitude;
+		}
+		public String getFormattedLatitude() {
+			return String.format(Locale.ENGLISH, "%.7f", _latitude).trim();
+		}
+		public double getLongitude() {
+			return _longitude;
+		}
+		public String getFormattedLongitude() {
+			return String.format(Locale.ENGLISH, "%.7f", _longitude).trim();
+		}
+		@Override
+		public String toString() {
+			return String.format(Locale.ENGLISH, "%.7f, %.7f", _latitude,  _longitude);
+		}
+	}
 	/**
 	 * find the start index where GPS longitude and latitude has coordinate data
 	 * @param recordSet
@@ -400,4 +429,185 @@ public class GPSHelper {
 		if(log.isLoggable(Level.TIME)) log.log(Level.TIME, "calcualation time = " + StringHelper.getFormatedTime("ss:SSS", (new Date().getTime() - startTime)));//$NON-NLS-1$ //$NON-NLS-2$	
 		return recordAzimuth;
 	}
+
+	/**
+	 * calculate labs based on GPS coordinates, start line will be calculated using launch point and first closes pass
+	 * this assumes valid GPS data from the beginning
+	 * @param device
+	 * @param recordSet
+	 * @param recordOrdinalLatitude
+	 * @param recordOrdinalLongitude
+	 * @param recordOrdinalDistance
+	 * @return
+	 */
+	public static synchronized Vector<Long> calculateLabs(final IDevice device, final RecordSet recordSet, final int recordOrdinalLatitude, final int recordOrdinalLongitude, final int recordOrdinalDistance, final int recordOrdinalTrip, final int recordOrdinalSpeed) {
+		Vector<Long> labTimes = new Vector<Long>();
+		long startTime = new Date().getTime();
+		
+		try {
+			//input records
+			Record recordLatitude = recordSet.get(recordOrdinalLatitude);
+			Record recordLongitude = recordSet.get(recordOrdinalLongitude);
+			Record recordDistance = recordSet.get(recordOrdinalDistance);
+			Record recordSpeed = recordSet.get(recordOrdinalSpeed);
+			Record recordTrip = recordOrdinalTrip >= 0 ? recordSet.get(recordOrdinalTrip) : null;
+			int recordSize = recordLatitude.realSize();
+
+			if (recordLatitude.hasReasonableData() && recordLongitude.hasReasonableData()) {
+				//check GPS latitude and longitude				
+				int indexGPS = 0;
+				int i = 0;
+				for (; i < recordSize; ++i) {
+					if (recordLatitude.get(i) != 0 && recordLongitude.get(i) != 0) {
+						indexGPS = i;
+						break;
+					}
+				}
+
+				//calculate index delta for 12/2 sec filter time
+				int filterIndexCount = (int) (6000 / (recordSet.getTime_ms(recordSize - 1) / recordSize));
+				
+				//find near by visit point
+				int minDistance = recordDistance.get(i);
+				int maxDistance = recordDistance.get(i);
+				int tempDistanceStart = 0;
+				double lapStartTime;
+				int lapStartTrip;
+				
+//				while (i < recordSize-filterIndexCount && ((tempDistanceStart = recordDistance.get(i)) >= maxDistance || recordDistance.get(i+filterIndexCount) >= maxDistance)) {
+//					maxDistance = tempDistanceStart;
+//					++i;
+//				}
+//				log.log(Level.OFF, recordSet.getFormatedTime_sec(i, false) + " maxDistance = " + device.translateValue(recordDistance, maxDistance/1000.0)); //$NON-NLS-1$
+//				i+=filterIndexCount; //time filter to short lap
+//									
+//				minDistance = maxDistance;
+//				int equalCounter = 0;
+//				while (i < recordSize-filterIndexCount && ((tempDistanceStart = recordDistance.get(i)) <= minDistance || recordDistance.get(i+filterIndexCount) <= minDistance)) {
+//					if (minDistance == tempDistanceStart) equalCounter++;
+//					else equalCounter = 0;
+//					minDistance = tempDistanceStart;
+//					++i;
+//				}
+//				log.log(Level.OFF, recordSet.getFormatedTime_sec(i, false) + " minDistance = " + device.translateValue(recordDistance, minDistance/1000.0));//$NON-NLS-1$
+//				i-=equalCounter/2;
+				
+				lapStartTime = recordSet.getTime_ms(i);
+				lapStartTrip = recordTrip != null ? recordTrip.get(i) : 0;
+				
+				//calculate index delta for 12 sec filter time
+				filterIndexCount = (int) (20*1.8); //(int) (20000 / (recordSet.getTime_ms(recordSize - 1) / recordSize));
+
+			
+				//create the start line between start point and next nearest point.
+//				final LatLong tmpStartPoint = new LatLong(round(device.translateValue(recordLatitude, recordLatitude.get(indexGPS)/1000.0)), round(device.translateValue(recordLongitude, recordLongitude.get(indexGPS)/1000.0)));
+//				final LatLong tmpMinPoint = new LatLong(round(device.translateValue(recordLatitude, recordLatitude.get(i)/1000.0)), round(device.translateValue(recordLongitude, recordLongitude.get(i)/1000.0)));
+//				double deltaLatitude = tmpMinPoint.getLatitude()-tmpStartPoint.getLatitude();
+//				double deltaLongitude = tmpMinPoint.getLongitude()-tmpStartPoint.getLongitude();
+//				final LatLong startPoint = new LatLong(tmpStartPoint.getLatitude() - deltaLatitude * 2, tmpStartPoint.getLongitude() - deltaLongitude * 2);
+//				final LatLong endPoint = new LatLong(tmpStartPoint.getLatitude() + deltaLatitude * 3, tmpStartPoint.getLongitude() + deltaLongitude * 3);
+				final LatLong startPoint = new LatLong(48.630878451126456, 8.974716626107693);
+				final LatLong endPoint = new LatLong(48.631116656063156, 8.975400254130363);
+//				log.log(Level.OFF, "startLine - startPoint = " + startPoint.toString() + " endPoint = " + endPoint);
+				double deltaLatitude = Math.abs(endPoint.getLatitude()-startPoint.getLatitude());
+				double deltaLongitude = Math.abs(endPoint.getLongitude()-startPoint.getLongitude());
+				boolean isLatitude = deltaLatitude > deltaLongitude;
+				Map<String, Double> startLine = new TreeMap<String, Double>();
+				
+				if (isLatitude) {
+					//log.log(Level.OFF, String.format(Locale.ENGLISH, "isLatitude = true; delta = %03.7f", deltaLatitude));
+					final int count = Integer.valueOf(String.format(Locale.ENGLISH, "%.7f", deltaLatitude).substring(5));
+					final double deltaLat = deltaLatitude / count;
+					final double deltaLong = deltaLongitude / count;
+					if (startPoint.getLatitude() < endPoint.getLatitude())
+						for (int j = 0; j < count; j++) {
+							startLine.put(String.format(Locale.ENGLISH, "%.7f", (startPoint.getLatitude() + j*deltaLat)), (startPoint.getLongitude() + j*deltaLong));
+						}
+					else
+						for (int j = 0; j < count; j++) {
+							startLine.put(String.format(Locale.ENGLISH, "%.7f", (endPoint.getLatitude() + j*deltaLat)), (endPoint.getLongitude() + j*deltaLong));
+						}
+				} else {
+					//log.log(Level.OFF, String.format(Locale.ENGLISH, "isLongitude = true; delta = %03.7f", deltaLongitude));
+					final int count = Integer.valueOf(String.format(Locale.ENGLISH, "%.7f", deltaLongitude).substring(5));
+					final double deltaLat = deltaLatitude / count;
+					final double deltaLong = deltaLongitude / count;
+					if (startPoint.getLongitude() < endPoint.getLongitude())
+						for (int j = 0; j < count; j++) {
+							startLine.put(String.format(Locale.ENGLISH, "%.7f", (startPoint.getLongitude() + j*deltaLong)), (startPoint.getLatitude() + j*deltaLat));
+						}
+					else
+						for (int j = 0; j < count; j++) {
+							startLine.put(String.format(Locale.ENGLISH, "%.7f", (endPoint.getLongitude() + j*deltaLong)), (endPoint.getLatitude() + j*deltaLat));
+						}
+				}
+
+				i+=filterIndexCount; //time filter to short lap
+				++i;
+				for (int j = 0; i < recordSize; i++) { // j=0 for disabled start line detection based on nearest passing point else j=1
+					final LatLong lastLatLong = new LatLong(device.translateValue(recordLatitude, recordLatitude.get(i-1)/1000.0), device.translateValue(recordLongitude, recordLongitude.get(i-1)/1000.0));
+					final LatLong latLong = new LatLong(device.translateValue(recordLatitude, recordLatitude.get(i)/1000.0), device.translateValue(recordLongitude, recordLongitude.get(i)/1000.0));
+					final int speedFactor = 1 + recordSpeed.get(i)/ 1000 / 15; 
+					if (isLatitude) {
+						if (startLine.get(lastLatLong.getFormattedLatitude()) != null && startLine.get(latLong.getFormattedLatitude()) != null) { //startLine hit found
+							if (Math.abs(startLine.get(lastLatLong.getFormattedLatitude()) - lastLatLong.getLongitude()) <= 0.00006*speedFactor) {
+//								log.log(Level.OFF, String.format(Locale.ENGLISH, "%03.7f", Math.abs(Double.valueOf(startLine.get(lastLatLong.getFormattedLatitude())) - lastLatLong.getLongitude())) 
+//								+ " - " + String.format(Locale.ENGLISH, "%03.7f", Math.abs(Double.valueOf(startLine.get(latLong.getFormattedLatitude())) - latLong.getLongitude())));
+								if (Math.abs(startLine.get(latLong.getFormattedLatitude()) - latLong.getLongitude()) > 0.00002/speedFactor) {
+//									log.log(Level.OFF, String.format(Locale.ENGLISH, "%03.7f", Math.abs(Double.valueOf(startLine.get(lastLatLong.getFormattedLatitude())) - lastLatLong.getLongitude())) 
+//									+ " - " + String.format(Locale.ENGLISH, "%03.7f", Math.abs(Double.valueOf(startLine.get(latLong.getFormattedLatitude())) - latLong.getLongitude())));
+									if (j > 0)
+										log.log(Level.OFF, String.format(Locale.ENGLISH, "startLine hit at %s - lap %2d time = %s - speed = %4d km/h",
+													StringHelper.getFormatedTime("mm:ss.SSS", (long) (recordSet.getTime_ms(i))), 
+													j,
+													StringHelper.getFormatedTime("mm:ss.SSS", (long) (recordSet.getTime_ms(i) - lapStartTime)),
+													recordSpeed.get(i)/1000));
+//													(recordTrip != null ? recordTrip.get(i) : 0) - lapStartTrip));//$NON-NLS-1$
+									lapStartTime = recordSet.getTime_ms(i);
+									lapStartTrip = recordTrip != null ? recordTrip.get(i) : 0;
+									i+=filterIndexCount;//time filter to short lap
+									j+=1;
+								}
+							}
+						}
+					}
+					else {
+						if (startLine.get(lastLatLong.getFormattedLongitude()) != null && startLine.get(latLong.getFormattedLongitude()) != null) { //startLine hit found
+							if (Math.abs(startLine.get(lastLatLong.getFormattedLongitude()) - lastLatLong.getLatitude()) <= 0.00006*speedFactor) {
+//								log.log(Level.OFF, String.format(Locale.ENGLISH, "%03.7f", Math.abs(Double.valueOf(startLine.get(lastLatLong.getFormattedLongitude())) - lastLatLong.getLatitude())) 
+//										+ " - " + String.format(Locale.ENGLISH, "%03.7f", Math.abs(Double.valueOf(startLine.get(latLong.getFormattedLongitude())) - latLong.getLatitude())));
+								if (Math.abs(startLine.get(latLong.getFormattedLongitude()) - latLong.getLatitude()) > 0.00002/speedFactor) {
+//									log.log(Level.OFF, String.format(Locale.ENGLISH, "%03.7f", Math.abs(Double.valueOf(startLine.get(lastLatLong.getFormattedLongitude())) - lastLatLong.getLatitude())) 
+//									+ " - " + String.format(Locale.ENGLISH, "%03.7f", Math.abs(Double.valueOf(startLine.get(latLong.getFormattedLongitude())) - latLong.getLatitude())));
+									if (j > 0)
+										log.log(Level.OFF, String.format(Locale.ENGLISH, "startLine hit at %s - lap %2d time = %s - speed = %4d km/h",
+													StringHelper.getFormatedTime("mm:ss.SSS", (long) (recordSet.getTime_ms(i))), 
+													j,
+													StringHelper.getFormatedTime("mm:ss.SSS", (long) (recordSet.getTime_ms(i) - lapStartTime)),
+													recordSpeed.get(i)/1000));
+//													(recordTrip != null ? recordTrip.get(i) : 0) - lapStartTrip));//$NON-NLS-1$
+									lapStartTime = recordSet.getTime_ms(i);
+									lapStartTrip = recordTrip != null ? recordTrip.get(i) : 0;
+									i+=filterIndexCount;//time filter to short lap
+									j+=1;
+								}
+							}
+						}
+					}					
+				}
+			}
+		}
+		catch (RuntimeException e) {
+			log.log(Level.WARNING, e.getMessage(), e);
+		}
+		
+		if(log.isLoggable(Level.TIME)) log.log(Level.TIME, "calcualation time = " + StringHelper.getFormatedTime("ss:SSS", (new Date().getTime() - startTime)));//$NON-NLS-1$ //$NON-NLS-2$	
+		return labTimes;
+	}
+	
+//  final static double ORDER = Math.pow(10, 7);
+//	public static double round(final double value)  {
+//      return Math.round(value * ORDER) / ORDER;
+//  }
+
 }
