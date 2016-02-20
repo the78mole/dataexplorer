091373fa3ed5a9a3e4effd8635e02bee6570d465
Winfried Bruegmann <winfried.bruegmann@web.de>
2016-02-20 19:38:20 +0000

    add iMax B6 support

---
+++ eStation/src/gde/device/skyrc/IMaxB6.java
@@ -0,0 +1,50 @@
+/**************************************************************************************
+  	This file is part of GNU DataExplorer.
+
+    GNU DataExplorer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GNU DataExplorer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Copyright (c) 2016 Winfried Bruegmann
+****************************************************************************************/
+package gde.device.skyrc;
+
+import java.io.FileNotFoundException;
+
+import javax.xml.bind.JAXBException;
+
+import gde.device.DeviceConfiguration;
+import gde.device.bantam.eStationBC6;
+
+/**
+ * Pichler P6 device class which is 100% eStation BC6
+ * @author Winfried Brügmann
+ */
+public class IMaxB6 extends eStationBC6 {
+
+	/**
+	 * @param deviceProperties
+	 * @throws FileNotFoundException
+	 * @throws JAXBException
+	 */
+	public IMaxB6(String deviceProperties) throws FileNotFoundException, JAXBException {
+		super(deviceProperties);
+	}
+
+	/**
+	 * @param deviceConfig
+	 */
+	public IMaxB6(DeviceConfiguration deviceConfig) {
+		super(deviceConfig);
+	}
+
+}
