cb0512779fea1e4b496cb59bb83e341fa3ecd2a6
Winfried Bruegmann <winfried.bruegmann@web.de>
2016-09-04 09:52:54 +0000

    make Google Earth auto export more configurable, extrude, select some record

--- WStechVario/src/gde/device/wstech/DataVario.java
+++ WStechVario/src/gde/device/wstech/DataVario.java
@@ -635,7 +635,8 @@ public class DataVario  extends DeviceConfiguration implements IDevice {
 			if (activeRecordSet != null && fileEndingType.contains(GDE.FILE_ENDING_KMZ)) {
 				//0=Empfänger-Spannung 1=Höhe 2=Motor-Strom 3=Motor-Spannung 4=Motorakku-Kapazität 5=Geschwindigkeit 6=Temperatur 7=GPS-Länge 8=GPS-Breite 9=GPS-Höhe 10=GPS-Geschwindigkeit 11=Steigen 12=ServoImpuls
 				//13=tripLength 14=distance 15=azimuth 16=directionStart
-				exportFileName = new FileHandler().exportFileKMZ(7, 8, 9, 10, 11, 13, 15, true, isExportTmpDir);
+				final int additionalMeasurementOrdinal = this.getGPS2KMZMeasurementOrdinal();
+				exportFileName = new FileHandler().exportFileKMZ(7, 8, 9, additionalMeasurementOrdinal, 11, 13, 15, true, isExportTmpDir);
 			}
 		}
 		return exportFileName;
@@ -648,7 +649,10 @@ public class DataVario  extends DeviceConfiguration implements IDevice {
 	public Integer getGPS2KMZMeasurementOrdinal() {
 		//0=Empfänger-Spannung 1=Höhe 2=Motor-Strom 3=Motor-Spannung 4=Motorakku-Kapazität 5=Geschwindigkeit 6=Temperatur 7=GPS-Länge 8=GPS-Breite 9=GPS-Höhe 10=GPS-Geschwindigkeit 11=Steigen 12=ServoImpuls
 		//13=tripLength 14=distance 15=azimuth 16=directionStart
-		return 10; 
+		if (this.kmzMeasurementOrdinal == null) // keep usage as initial supposed and use speed measurement ordinal
+			return 10;
+
+		return this.kmzMeasurementOrdinal;
 	}
 	
 	/**
