f0a64720c557ee3eb711e981a555dfe879bed495
brueg <winfried.bruegmann@web.de>
2016-12-01 17:43:41 +0100

    rename dummy driver to make it open fto be used for all SkyRC devices working with USB

--- build/misc/MAC_OS_X_SkyRC_Dummy_USB_Driver/createDummyUsbDriver.sh
+++ build/misc/MAC_OS_X_SkyRC_Dummy_USB_Driver/createDummyUsbDriver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# ***** script to create a dummy USB driver to make SkyRC MC3000 USB adapter usable with MAC OS X ** 06 Dec 2015 WB *****
+# ***** script to create a dummy USB driver to make SkyRC device USB adapter usable with MAC OS X ** 01 Dec 2016 WB *****
 # execution: sudo createDummyUsbDriver.sh
-cp -r MC3000.kext /System/Library/Extensions/
+cp -r SkyRC.kext /System/Library/Extensions/
 cd /System/Library/Extensions
-chown -R root:wheel MC3000.kext
-chmod -R 755 MC3000.kext
+chown -R root:wheel SkyRC.kext
+chmod -R 755 SkyRC.kext
 kextcache -system-caches
-echo MC3000 dummy driver installed please reboot the system before connecting MC3000 again.
+echo SkyRC USB dummy driver installed please reboot the system before connecting a SKyRC USB device again.
