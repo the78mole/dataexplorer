92b164477b7cfc54c0e36e3952f1239b854ffa92
et <thomas_eickert@hotmail.com>
2018-03-02 15:14:15 +0100

    Restore template name display in the status bar
    A non-default template name is already shown in the status bar.
    This is a preferable solution as most users do not work with
    templates.
    
    Test option 1:
    - Save a history template with an arbitrary name
    - Check if the name is in the status bar
    Test option 2:
    - Load a history template with an arbitrary name
    - Check if the name is in the status bar
    Test option 3:
    - Restore the default history template
    - Check if the name disappears in the status bar

--- DataExplorer/src/gde/histo/recordings/TrailRecordSet.java
+++ DataExplorer/src/gde/histo/recordings/TrailRecordSet.java
@@ -82,8 +82,6 @@ import gde.histo.utils.GpsCoordinate;
 import gde.histo.utils.UniversalQuantile;
 import gde.log.Level;
 import gde.log.Logger;
-import gde.messages.MessageIds;
-import gde.messages.Messages;
 import gde.ui.DataExplorer;
 import gde.ui.SWTResourceManager;
 
@@ -1037,7 +1035,6 @@ public final class TrailRecordSet extends AbstractRecordSet {
 		template.setCommentSuffix(name + " " + description);
 		template.store();
 		log.fine(() -> "creating histo graphics template file in " + template.getTargetFilePath());
-		DataExplorer.getInstance().setStatusMessage(Messages.getString(MessageIds.GDE_MSGT0922, new String[] {template.getHistoFileName()}));
 	}
 
 	/**
@@ -1092,7 +1089,6 @@ public final class TrailRecordSet extends AbstractRecordSet {
 				presentHistoExplorer.getHistoSummaryTabItem().setChartWeights(chartWeights);
 			}
 			log.fine(() -> "applied histo graphics template file " + template.getTargetFilePath());
-			DataExplorer.getInstance().setStatusMessage(Messages.getString(MessageIds.GDE_MSGT0923, new String[] {template.getHistoFileName()}));
 
 			if (doUpdateVisibilityStatus) {
 				setDisplayable();
