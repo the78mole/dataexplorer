bfda9613f13b47d6fffc7dd51bbe1d39c3d285a2
brueg <winfried.bruegmann@web.de>
2018-03-11 20:44:14 +0100

    copyright update 2018

--- DataExplorer/src/gde/histo/transitions/Transition.java
+++ DataExplorer/src/gde/histo/transitions/Transition.java
@@ -14,7 +14,7 @@
  You should have received a copy of the GNU General Public License
  along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
 
- Copyright (c) 2017 Thomas Eickert
+ Copyright (c) 2017,2018 Thomas Eickert
 ****************************************************************************************/
 package gde.histo.transitions;
 
--- HoTTAdapter/src/gde/device/graupner/HoTTbinHistoReader.java
+++ HoTTAdapter/src/gde/device/graupner/HoTTbinHistoReader.java
@@ -14,8 +14,8 @@
     You should have received a copy of the GNU General Public License
     along with GNU DataExplorer.  If not, see <http://www.gnu.org/licenses/>.
 
-    Copyright (c) 2011,2012,2013,2014,2015,2016,2017 Winfried Bruegmann
-    					2016,2017 Thomas Eickert
+    Copyright (c) 2011,2012,2013,2014,2015,2016,2017,2018 Winfried Bruegmann
+    					2016,2017,2018 Thomas Eickert
 ****************************************************************************************/
 package gde.device.graupner;
 
