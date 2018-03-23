9c5b4dfeecca4b4878347999617aba16377a3a01
et <thomas_eickert@hotmail.com>
2018-03-23 14:57:32 +0100

    Add the equals method to the Channel class
    Reason is that different channels have been identified as equal.
    This happens if the channel size is zero which is a common case in histo
    analysis.

--- DataExplorer/src/gde/data/Channel.java
+++ DataExplorer/src/gde/data/Channel.java
@@ -796,5 +796,29 @@ public class Channel extends HashMap<String, RecordSet> {
 		super.clear();
 		this.objectKey = GDE.STRING_EMPTY;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((this.name == null) ? 0 : this.name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (!super.equals(obj)) return false;
+		if (getClass() != obj.getClass()) return false;
+		Channel other = (Channel) obj;
+		if (this.name == null) {
+			if (other.name != null) return false;
+		} else if (!this.name.equals(other.name)) {
+			return false;
+		} else {
+			super.equals(other);
+		}
+		return true;
+	}
 }
 
