459241218f09c7df504a22d8cb438d44300e35e2
brueg <winfried.bruegmann@web.de>
2018-11-13 18:20:11 +0100

    fix missing java library references

--- DataExplorer/src/gde/histo/datasources/SourceDataSet.java
+++ DataExplorer/src/gde/histo/datasources/SourceDataSet.java
@@ -32,7 +32,7 @@ import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.stream.Stream;
 
-import com.sun.istack.internal.Nullable;
+import com.sun.istack.Nullable;
 
 import gde.Analyzer;
 import gde.GDE;
