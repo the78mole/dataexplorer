b8cf54cda6cf7c11849ebb79106c93151dd5c7c9
brueg <winfried.bruegmann@web.de>
2019-04-09 11:14:17 +0200

    add DataExplorer.cmd script to Windows installation to be used while openJDK is installed and java.exe in PATH

---
+++ build/misc/DataExplorer32.cmd
@@ -0,0 +1,3 @@
+@REM ***** script to launch DataExplorer ** 09 Apr 2019 WB *****
+@echo java -Dfile.encoding=UTF-8 -jar -Xms64m -Xmx1280m ./DataExplorer.jar
+@java -Dfile.encoding=UTF-8 -jar -Xms64m -Xmx1280m ./DataExplorer.jar
---
+++ build/misc/DataExplorer64.cmd
@@ -0,0 +1,3 @@
+@REM ***** script to launch DataExplorer ** 09 Apr 2019 *****
+@echo java -Dfile.encoding=UTF-8 -jar -Xms64m -Xmx3092m ./DataExplorer.jar
+@java -Dfile.encoding=UTF-8 -jar -Xms64m -Xmx3092m ./DataExplorer.jar
