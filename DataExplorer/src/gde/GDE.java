285502636a6c643c95cbeb77351f32abe7cf76b3
brueg <winfried.bruegmann@web.de>
2020-04-10 18:48:58 +0200

    enable beta version display

--- DataExplorer/src/gde/GDE.java
+++ DataExplorer/src/gde/GDE.java
@@ -108,8 +108,10 @@ public class GDE {
 	private static boolean									isWithUi													= false;
 
 	// ****** begin global constants section *******
-	public static final String							VERSION														= "Version 3.4.1";																																																					//$NON-NLS-1$
-	public static final int									VERSION_NUMBER										= Integer.parseInt(GDE.VERSION.substring(8).replace(GDE.STRING_DOT, GDE.STRING_EMPTY));
+	public static final String							VERSION														= "Version 3.4.2 beta 7";																																																					//$NON-NLS-1$
+	public static final int									VERSION_NUMBER										= GDE.VERSION.contains("beta") 
+															? Integer.parseInt(GDE.VERSION.substring(8, 8+5).replace(GDE.STRING_DOT, GDE.STRING_EMPTY)) - 1
+															: Integer.parseInt(GDE.VERSION.substring(8, 8+5).replace(GDE.STRING_DOT, GDE.STRING_EMPTY));
 	public static final String							NAME_SHORT												= "GDE";																																																										//$NON-NLS-1$
 	public static final String							NAME_LONG													= "DataExplorer";																																																						//$NON-NLS-1$
 	public final static String							DEVICE_PROPERTIES_XSD_VERSION			= "_V39";																																																										//$NON-NLS-1$
