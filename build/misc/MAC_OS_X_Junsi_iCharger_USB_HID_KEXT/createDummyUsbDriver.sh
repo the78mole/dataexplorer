147ec3325a64040c34d68212f306358351067c01
brueg <winfried.bruegmann@web.de>
2020-04-13 18:45:55 +0200

    fix create dummy USB driver kext

--- build/misc/MAC_OS_X_Junsi_iCharger_USB_HID_KEXT/createDummyUsbDriver.sh
+++ build/misc/MAC_OS_X_Junsi_iCharger_USB_HID_KEXT/createDummyUsbDriver.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # ***** script to install kernel extension to make Junsi iCHarger x6/x8/308Duo/406Duo/4010Duo USB-HID adapter usable with MAC OS X ** 06 Mar 2019 WB *****
 # execution: sudo ./createDummyUsbDriver.sh
-cp -r iChargerDuo.kext /System/Library/Extensions/
+cp -r iChargerUSB.kext /System/Library/Extensions/
 cd /System/Library/Extensions
-chown -R root:wheel iChargerDuo.kext
-chmod -R 755 iChargerDuo.kext
+chown -R root:wheel iChargerUSB.kext
+chmod -R 755 iChargerUSB.kext
 kextcache -system-caches
 echo After iCharger kernel extension installed please reboot the system before connecting iCharger with USB-HID again.
 echo MAC OS X >= 10.13 High Sierra kernel extensions might be blocked, reboot into recovery console "(push cmd+R while boot)" open terminal
 echo enter "spctl kext-consent disable" and "csrutil disable", reboot to activate
-echo open terminal enter "sudo kextutil -t /System/Library/Extensions/iChargerDuo.kext" before USB cable connected. This will load none signed kext
+echo open terminal enter "sudo kextutil -t /System/Library/Extensions/iChargerUSB.kext" before USB cable connected. This will load none signed kext
 echo ignore warnings regarding missing code signing error 
-echo alternatively use /Library/Extensions directory if /System/Library/Extensions is read-only file system (catalina)
-echo Alternative use Kext Utility to copy iChargerDuo.kext os search the internet for installing 3rd party kext
+echo alternatively use /Library/Extensions directory if /System/Library/Extensions is read-only file system "(Catalina)"
+echo Alternative use Kext Utility to copy iChargerUSB.kext os search the internet for installing 3rd party kext
 echo Kext Utiliy may help installing kernel extension, search internet for download link
 echo Unfortunately, "kext-dev-mode=1" and "rootless=0" these keys are no longer working for future versions of Mac OS X starting with El Capitan GM. 
 echo You need disable SIP with Clover Bootloader.
--- build/misc/MAC_OS_X_SkyRC_USB_HID_KEXT/createDummyUsbDriver.sh
+++ build/misc/MAC_OS_X_SkyRC_USB_HID_KEXT/createDummyUsbDriver.sh
@@ -9,9 +9,9 @@ kextcache -system-caches
 echo After SkyRC USB kernel extension installed please reboot the system before connecting a SKyRC USB device again.
 echo MAC OS X >= 10.13 High Sierra kernel extensions might be blocked, reboot into recovery console "(push cmd+R while boot)" open terminal
 echo enter "spctl kext-consent disable" and "csrutil disable", reboot to activate
-echo open terminal enter "sudo kextutil -t /System/Library/Extensions/iChargerDuo.kext" before USB cable connected. This will load none signed kext
+echo open terminal enter "sudo kextutil -t /System/Library/Extensions/SkyRC.kext" before USB cable connected. This will load none signed kext
 echo ignore warnings regarding missing code signing error 
-echo alternatively use /Library/Extensions directory if /System/Library/Extensions is read-only file system (catalina)
+echo alternatively use /Library/Extensions directory if /System/Library/Extensions is read-only file system "(Catalina)"
 echo Alternative use Kext Utility to copy iChargerDuo.kext os search the internet for installing 3rd party kext
 echo Kext Utiliy may help installing kernel extension, search internet for download link
 echo Unfortunately, "kext-dev-mode=1" and "rootless=0" these keys are no longer working for future versions of Mac OS X starting with El Capitan GM. 
