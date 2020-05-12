d61bb5a17a1a8b514e0306d429951a5e4d909a17
brueg <winfried.bruegmann@web.de>
2020-05-12 14:50:04 +0200

    refine macOS related USB HID description

--- build/misc/MAC_OS_X_Junsi_iCharger_USB_HID_KEXT/createDummyUsbDriver.sh
+++ build/misc/MAC_OS_X_Junsi_iCharger_USB_HID_KEXT/createDummyUsbDriver.sh
@@ -6,13 +6,13 @@ cd /Library/Extensions
 chown -R root:wheel iChargerUSB.kext
 chmod -R 755 iChargerUSB.kext
 kextcache -system-caches
-echo The kernel extension does not contain any code, it only prevent system to allocate USB HID and enable permission for user
+echo The kernel extension does not contain any code, it only prevent system to allocate USB HID and enable permission for user context.
 echo After iCharger kernel extension installed please reboot the system before connecting iCharger with USB-HID again.
 echo MAC OS X >= 10.13 High Sierra kernel extensions might be blocked, reboot into recovery console "(push cmd+R while boot)" open terminal
-echo enter "spctl kext-consent disable" and "csrutil disable", reboot to activate
-echo open terminal enter "sudo kextutil -t -v 1 /Library/Extensions/iChargerUSB.kext" before USB cable connected. This will load none signed kext
-echo ignore warnings regarding missing code signing error 
-echo alternatively use /Library/Extensions directory if /System/Library/Extensions is read-only file system "(Catalina)" or "sudo mount -rw /"
+echo Enter "spctl kext-consent disable" and "csrutil disable", reboot to activate
+echo Open terminal enter "sudo kextutil -t -v 1 /Library/Extensions/iChargerUSB.kext" each time before USB cable connected. 
+echo This will load none signed kext and enable user claim, ignore warnings regarding missing code signing 
+echo Alternatively use /Library/Extensions directory if /System/Library/Extensions is read-only file system "(Catalina)" or "sudo mount -rw /"
 echo Alternative use Kext Utility or KextBeast to copy iChargerUSB.kext os search the Internet for installing 3rd party kext
 echo Kext Utiliy may help installing kernel extension, search internet for download link
 echo Unfortunately, "kext-dev-mode=1" and "rootless=0" these keys are no longer working for future versions of Mac OS X starting with El Capitan GM. 
--- build/misc/MAC_OS_X_SkyRC_USB_HID_KEXT/createDummyUsbDriver.sh
+++ build/misc/MAC_OS_X_SkyRC_USB_HID_KEXT/createDummyUsbDriver.sh
@@ -6,13 +6,13 @@ cd /Library/Extensions
 chown -R root:wheel SkyRC.kext
 chmod -R 755 SkyRC.kext
 kextcache -system-caches
-echo The kernel extension does not contain any code, it only prevent system to allocate USB HID and enable permission for user
+echo The kernel extension does not contain any code, it only prevent system to allocate USB HID and enable permission for user context.
 echo After SkyRC USB kernel extension installed please reboot the system before connecting a SKyRC USB device again.
 echo MAC OS X >= 10.13 High Sierra kernel extensions might be blocked, reboot into recovery console "(push cmd+R while boot)" open terminal
-echo enter "spctl kext-consent disable" and "csrutil disable", reboot to activate
-echo open terminal enter "sudo kextutil -t -v 1 /Library/Extensions/SkyRC.kext" before USB cable connected. This will load none signed kext
-echo ignore warnings regarding missing code signing error 
-echo use /Library/Extensions directory if /System/Library/Extensions is read-only file system "(Catalina)" or "sudo mount -rw /"
+echo Enter "spctl kext-consent disable" and "csrutil disable", reboot to activate
+echo Open terminal enter "sudo kextutil -t -v 1 /Library/Extensions/SkyRC.kext" each time before USB cable connected. 
+echo This will load none signed kext, ignore warnings regarding missing code signing error 
+echo Use /Library/Extensions directory if /System/Library/Extensions is read-only file system "(Catalina)" or "sudo mount -rw /"
 echo Alternative use Kext Utility or KextBeast to copy SkyRC.kext os search the Internet for installing 3rd party kext
 echo Kext Utiliy may help installing kernel extension, search internet for download link
 echo Unfortunately, "kext-dev-mode=1" and "rootless=0" these keys are no longer working for future versions of Mac OS X starting with El Capitan GM. 
