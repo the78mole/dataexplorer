5361c2206386fab4e700aedd61716b83855ec707
brueg <winfried.bruegmann@web.de>
2021-06-14 14:23:40 +0200

    prepare version 3.4.9

--- Pulsar/src/gde/device/elprog/Pulsar3.java
+++ Pulsar/src/gde/device/elprog/Pulsar3.java
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with GNU DataExplorer.  If not, see <https://www.gnu.org/licenses/>.
 
-    Copyright (c) 2017,2018,2019,2020 Winfried Bruegmann
+    Copyright (c) 2017,2018,2019,2020,2021 Winfried Bruegmann
 ****************************************************************************************/
 package gde.device.elprog;
 
