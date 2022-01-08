a08006beed53a99387cdba7ba9a5894ba06c3422
brueg <winfried.bruegmann@web.de>
2022-01-08 18:36:04 +0100

    prepare beta 3, change JRE check 1.8-17

--- DataExplorer/src/gde/GDE.java
+++ DataExplorer/src/gde/GDE.java
@@ -530,7 +530,7 @@ public class GDE {
 			GDE.seStartupProgress(100);
 			int javaVmSpecificationVersion = Integer.parseInt(((String)props.get("java.vm.specification.version")).contains(GDE.STRING_DOT) 
 					? ((String)props.get("java.vm.specification.version")).substring(2) : ((String)props.get("java.vm.specification.version")));
-			if (javaVmSpecificationVersion < 8 || javaVmSpecificationVersion > 11) 
+			if (javaVmSpecificationVersion < 8 || javaVmSpecificationVersion > 17) 
 				application.openMessageDialog(Messages.getString(MessageIds.GDE_MSGW0050, new Integer[] {javaVmSpecificationVersion}));
 			application.execute(inputFilePath);
 		}
